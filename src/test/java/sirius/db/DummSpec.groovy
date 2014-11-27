fe1868a20de419760c344663f5ae443cccaebfec
aha <aha@scireum.de>
2014-11-27 19:18:24 +0100

    Added dummy-test

---
+++ src/test/java/sirius/db/DummSpec.groovy
@@ -0,0 +1,22 @@
+/*
+ * Made with all the love in the world
+ * by scireum in Remshalden, Germany
+ *
+ * Copyright by scireum GmbH
+ * http://www.scireum.de - info@scireum.de
+ */
+
+package sirius.db
+
+import sirius.kernel.BaseSpecification
+
+/**
+ * Here as long as there are no real tests...
+ */
+class DummSpec extends BaseSpecification {
+
+    def dummy() {
+
+    }
+
+}
