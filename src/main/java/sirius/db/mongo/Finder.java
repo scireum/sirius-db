14afdc09f9531b9cb361887801466ae77515928c
aha <aha@scireum.de>
2016-07-01 14:03:03 +0200

    Added helper methods to limit a query and to count its  results

--- src/main/java/sirius/db/mongo/Finder.java
+++ src/main/java/sirius/db/mongo/Finder.java
@@ -26,6 +26,8 @@ public class Finder extends QueryBuilder<Finder> {
 
     private BasicDBObject fields;
     private BasicDBObject orderBy;
+    private int skip;
+    private int limit;
 
     protected Finder(Mongo mongo) {
         super(mongo);
@@ -77,6 +79,35 @@ public class Finder extends QueryBuilder<Finder> {
     }
 
     /**
+     * Adds a limit to the query.
+     *
+     * @param skip  the number of items to skip (used for pagination).
+     * @param limit the max. number of items to return (exluding those who have been skipped).
+     * @return the builder itself for fluent method calls
+     */
+    public Finder limit(int skip, int limit) {
+        this.skip = skip;
+        this.limit = limit;
+
+        return this;
+    }
+
+    /**
+     * Adds a limit to the query.
+     * <p>
+     * This is boilerplate for {@code finder.limit(0, limit)}.
+     *
+     * @param limit the max. number of items to return
+     * @return the builder itself for fluent method calls
+     */
+    public Finder limit(int limit) {
+        this.skip = 0;
+        this.limit = limit;
+
+        return this;
+    }
+
+    /**
      * Executes the query for the given collection and returns a single document.
      *
      * @param collection the collection to search in
@@ -109,9 +140,14 @@ public class Finder extends QueryBuilder<Finder> {
      */
     public void eachIn(String collection, Function<Document, Boolean> processor) {
         Watch w = Watch.start();
+
         DBCursor cur = mongo.db().getCollection(collection).find(filterObject, fields);
         if (orderBy != null) {
             cur.sort(orderBy);
+        }
+        cur.skip(skip);
+        if (limit > 0) {
+            cur.limit(limit);
         }
         handleTracingAndReporting(collection, w, cur);
 
@@ -144,5 +180,27 @@ public class Finder extends QueryBuilder<Finder> {
             processor.accept(d);
             return true;
         });
+    }
+
+    /**
+     * Counts the number of documents in the result of the given query.
+     * <p>
+     * Note that limits are ignored for this query.
+     *
+     * @param collection the collection to search in
+     * @return the number of documents found
+     */
+    public long countIn(String collection) {
+        Watch w = Watch.start();
+        try {
+            DBCursor cur = mongo.db().getCollection(collection).find(filterObject, fields);
+            return cur.count();
+        } finally {
+            mongo.callDuration.addValue(w.elapsedMillis());
+            if (Microtiming.isEnabled()) {
+                w.submitMicroTiming("mongo", "COUNT - " + collection + ": " + filterObject);
+            }
+            traceIfRequired(collection, w);
+        }
     }
 }
