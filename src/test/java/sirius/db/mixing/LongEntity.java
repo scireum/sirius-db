455a3f08b59aa6f1a6c47349cd7770656d033236
aha <aha@scireum.de>
2016-10-20 09:21:07 +0200

    Makes a long property use BIGINT as database type
    An INT cannot store long values.

---
+++ src/test/java/sirius/db/mixing/LongEntity.java
@@ -0,0 +1,22 @@
+/*
+ * Made with all the love in the world
+ * by scireum in Remshalden, Germany
+ *
+ * Copyright by scireum GmbH
+ * http://www.scireum.de - info@scireum.de
+ */
+
+package sirius.db.mixing;
+
+public class LongEntity extends Entity {
+
+    private long longValue;
+
+    public long getLongValue() {
+        return longValue;
+    }
+
+    public void setLongValue(long longValue) {
+        this.longValue = longValue;
+    }
+}
