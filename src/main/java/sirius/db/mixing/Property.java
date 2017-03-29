fecb774e70bbbcbda08c5be955a944cd0ff12afe
Tobias Bischoff <tbi@scireum.de>
2017-03-29 09:38:49 +0200

    Catches parsing exceptions and handles them

--- src/main/java/sirius/db/mixing/Property.java
+++ src/main/java/sirius/db/mixing/Property.java
@@ -377,7 +377,12 @@ public abstract class Property {
      * @param value the value to parse and apply
      */
     public void parseValue(Entity e, Value value) {
-        setValue(e, transformValue(value));
+        try {
+            setValue(e, transformValue(value));
+        } catch (IllegalArgumentException exception) {
+            Exceptions.ignore(exception);
+            throw Exceptions.handle().withSystemErrorMessage(exception.getMessage()).handle();
+        }
     }
 
     /**
