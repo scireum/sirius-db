976aa3eacae1fabe813c4731b589c3aedd196fd0
aha <aha@scireum.de>
2018-11-08 11:06:40 +0100

    Removes the test scenario for elasticsearch-latest.
    The tag was droppend anyway. Also ES introduced non-backwards comaptible
    changes again, so once we want to upgrade we have to accomodate them first.

--- src/test/java/TestSuite.java
+++ src/test/java/TestSuite.java
@@ -19,10 +19,8 @@ import sirius.kernel.ScenarioSuite;
 @Scenario(file = "test-percona-latest.conf",
         includes = "sirius\\.db\\.jdbc.*",
         excludes = "sirius\\.db\\.jdbc.clickhouse.*")
-//@Scenario(file = "test-postgres-latest.conf", filter = "sirius\\.db\\.(jdbc).*")
 @Scenario(file = "test-redis-latest.conf", includes = "sirius\\.db\\.redis.*")
 @Scenario(file = "test-mongo-latest.conf", includes = "sirius\\.db\\.mongo.*")
-@Scenario(file = "test-elasticsearch-latest.conf", includes = "sirius\\.db\\.es.*")
 public class TestSuite {
 
 }
