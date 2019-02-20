60d8425495aeeba08617cf96e9f1fcfb515409b1
Tobias Bagg <tba@scireum.de>
2019-02-20 09:45:27 +0100

    Provides tests for reindexing and index aliasing

---
+++ src/test/java/sirius/db/es/ReindexSpec.groovy
@@ -0,0 +1,49 @@
+/*
+ * Made with all the love in the world
+ * by scireum in Remshalden, Germany
+ *
+ * Copyright by scireum GmbH
+ * http://www.scireum.de - info@scireum.de
+ */
+
+package sirius.db.es
+
+import sirius.kernel.BaseSpecification
+import sirius.kernel.commons.Wait
+import sirius.kernel.di.std.Part
+
+class ReindexSpec extends BaseSpecification {
+
+    @Part
+    private static Elastic elastic
+
+    def "reindex and move alias works"() {
+        given:
+        ElasticTestEntity e = new ElasticTestEntity()
+        e.setAge(10)
+        e.setFirstname("test")
+        e.setLastname("test")
+        and:
+        elastic.update(e)
+        and:
+        Wait.seconds(2)
+        when:
+        elastic.getLowLevelClient().reindex(e.getDescriptor(), "reindex-test")
+        then:
+        elastic.getLowLevelClient().indexExists("reindex-test")
+        when:
+        List indicesForAlias = elastic.getLowLevelClient().getIndicesForAlias(e.getDescriptor())
+        then:
+        indicesForAlias.size() == 1 && indicesForAlias.contains("elastictestentity")
+        when:
+        elastic.getLowLevelClient().moveActiveAlias(e.getDescriptor(), "reindex-test")
+        and:
+        indicesForAlias = elastic.getLowLevelClient().getIndicesForAlias(e.getDescriptor())
+        then:
+        indicesForAlias.size() == 1 && indicesForAlias.contains("reindex-test")
+        and:
+        Wait.seconds(2)
+        and:
+        elastic.select(ElasticTestEntity.class).eq(ElasticTestEntity.AGE, 10).queryOne().getFirstname() == "test"
+    }
+}
