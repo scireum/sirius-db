7c93947a8f5c6aa62d9caf3ab422379886c49778
aha <aha@scireum.de>
2020-05-20 13:42:29 +0200

    Provides blockwise query result processors for very large result sets.

--- src/main/java/sirius/db/jdbc/SmartQuery.java
+++ src/main/java/sirius/db/jdbc/SmartQuery.java
@@ -21,6 +21,7 @@ import sirius.kernel.async.TaskContext;
 import sirius.kernel.commons.Explain;
 import sirius.kernel.commons.Limit;
 import sirius.kernel.commons.Monoflop;
+import sirius.kernel.commons.Timeout;
 import sirius.kernel.commons.Tuple;
 import sirius.kernel.commons.Value;
 import sirius.kernel.commons.Watch;
@@ -34,6 +35,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -41,7 +43,10 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.TreeMap;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.function.Consumer;
 import java.util.function.Predicate;
 
 /**
@@ -51,6 +56,9 @@ import java.util.function.Predicate;
  */
 public class SmartQuery<E extends SQLEntity> extends Query<SmartQuery<E>, E, SQLConstraint> {
 
+    private static final Duration QUERY_ITERATE_TIMEOUT = Duration.ofMinutes(15);
+
+    
     @Part
     private static OMA oma;
 
@@ -183,6 +191,117 @@ public class SmartQuery<E extends SQLEntity> extends Query<SmartQuery<E>, E, SQL
     @Override
     public void delete() {
         iterateAll(oma::delete);
+
+    /**
+     * Calls the given function on all items in the result, as long as it returns <tt>true</tt>.
+     * <p>
+     * In contrast to {@link #iterate(Predicate)}, this method is suitable for large result sets or log processing
+     * times. As <tt>iterate</tt> keeps the JDBC <tt>ResultSet</tt> open, the underlying server might discard the
+     * result at some point in time (e.g. MySQL does this after 30-45min). Therefore, we execute the query and start
+     * processing. If a timeout ({@link #QUERY_ITERATE_TIMEOUT} is reached, we stop iterating, discard the result set
+     * and emit another query which starts just where the previous query stopped.
+     * <p>
+     * Note however, as we either do this by ID or by setting an appropriate LIMIT window, there is a possibility,
+     * that we either miss an entity or even process an entity twice if a concurrent modification happens, which
+     * then changes the result set of this query.
+     *
+     * @param handler the handler to be invoked for each item in the result
+     */
+    public void iterateBlockwise(Predicate<E> handler) {
+        if (orderBys.isEmpty()) {
+            iterateBlockwiseById(handler);
+        } else {
+            iterateBlockwiseByPaging(handler);
+        }
+    }
+
+    /**
+     * Calls the given function on all items in the result.
+     *
+     * @param handler the handler to be invoked for each item in the result
+     * @see #iterateBlockwise(Predicate)
+     */
+    public void iterateBlockwiseAll(Consumer<E> handler) {
+        iterateBlockwise(entity -> {
+            handler.accept(entity);
+            return true;
+        });
+    }
+
+    /**
+     * Provides a blockwise strategy based on the {@link SQLEntity#ID} of the entities.
+     * <p>
+     * If there are no ORDER BY clauses present, we can sort by ID and remember the last processed ID before
+     * attempting another query.
+     *
+     * @param predicate the handler to be invoked for each item in the result
+     */
+    private void iterateBlockwiseById(Predicate<E> predicate) {
+        AtomicLong lastId = new AtomicLong(-1);
+        AtomicBoolean keepGoing = new AtomicBoolean(true);
+        TaskContext context = TaskContext.get();
+        while (keepGoing.get() && context.isActive()) {
+            keepGoing.set(false);
+            Timeout timeout = new Timeout(QUERY_ITERATE_TIMEOUT);
+
+            // Creates a copy and start processing results just after the results we have processed with the
+            // previous query...
+            copy().orderAsc(SQLEntity.ID).where(OMA.FILTERS.gt(SQLEntity.ID, lastId.get())).iterate(entity -> {
+                if (!predicate.test(entity)) {
+                    // As soon as the handler returns false, we're done and can abort entirely...
+                    return false;
+                }
+
+                if (timeout.isReached()) {
+                    // If the timeout is reached, we set a flag so that another query will be attempted....
+                    keepGoing.set(true);
+                    // We remember the ID of the last processed entity, so that the query starts right after
+                    // this one...
+                    lastId.set(entity.getId());
+                    // Abort local processing so that another result set is opened.
+                    return false;
+                }
+
+                // Timeout not reached yet, continue processing results...
+                return true;
+            });
+        }
+    }
+
+    /**
+     * Provides a blockwise strategy based on the LIMIT window.
+     * <p>
+     * If ORDER BY clauses are present, we have to employ a sliding window technique.
+     *
+     * @param predicate the handler to be invoked for each item in the result
+     */
+    private void iterateBlockwiseByPaging(Predicate<E> predicate) {
+        // Contains the counter of already processed entities. These have to be skippend when emitting the
+        // next query...
+        AtomicInteger skipCounter = new AtomicInteger(0);
+        AtomicBoolean keepGoing = new AtomicBoolean(true);
+        TaskContext context = TaskContext.get();
+        while (keepGoing.get() && context.isActive()) {
+            keepGoing.set(false);
+            Timeout timeout = new Timeout(QUERY_ITERATE_TIMEOUT);
+            // Create a copy of the query an install an appropriate skip value...
+            copy().skip(skipCounter.get()).iterate(entity -> {
+                if (!predicate.test(entity)) {
+                    // As soon as the handler returns false, we're done and can abort entirely...
+                    return false;
+                }
+
+                // Remember the processed entity...
+                skipCounter.incrementAndGet();
+
+                if (timeout.isReached()) {
+                    keepGoing.set(true);
+                    return false;
+                }
+
+                return true;
+            });
+        }
     }
 
     @Override
