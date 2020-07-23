f6efe664d5aa5fbc7084599bc30fd0bf84132c01
Marc Egenrieder <meg@scireum.de>
2020-07-23 17:37:56 +0200

    Extends FieldLookupCacheSpec to also include mongo tests

--- src/test/java/sirius/db/jdbc/FieldLookupCacheSpec.groovy
+++ src/test/java/sirius/db/jdbc/FieldLookupCacheSpec.groovy
@@ -6,12 +6,17 @@
  * http://www.scireum.de - info@scireum.de
  */
 
-package sirius.db.jdbc
+package sirius.db.mixing
 
+import sirius.db.jdbc.OMA
+import sirius.db.jdbc.SmartQueryTestEntity
 import sirius.db.mixing.FieldLookupCache
 import sirius.db.mongo.Mango
+import sirius.db.mongo.MangoTestEntity
 import sirius.kernel.BaseSpecification
 import sirius.kernel.di.std.Part
+
+import java.time.LocalDateTime
 
 class FieldLookupCacheSpec extends BaseSpecification {
 
@@ -19,9 +24,12 @@ class FieldLookupCacheSpec extends BaseSpecification {
     private static OMA oma
 
     @Part
+    private static Mango mango
+
+    @Part
     private static FieldLookupCache lookupCache
 
-    def "lookup works"() {
+    def "jdbc field lookup works"() {
         given:
         SmartQueryTestEntity entity = new SmartQueryTestEntity()
         entity.setValue("Cache Test")
@@ -35,6 +43,33 @@ class FieldLookupCacheSpec extends BaseSpecification {
         value1 == "Cache Test"
         value2 == 12345
         value3 == "Cache Test"
+    }
+
+    def "mongo field lookup works"() {
+        given:
+        def localDateTime = LocalDateTime.now().minusYears(50)
+        MangoTestEntity mangoTest = new MangoTestEntity()
+        mangoTest.setFirstname("Gordon")
+        mangoTest.setLastname("Ramsay")
+        mangoTest.setAge(50)
+        mangoTest.setBirthday(localDateTime)
+        mangoTest.setCool(true)
+        mangoTest.getSuperPowers().modify().addAll(Arrays.asList("Cooking", "Raging"))
+        mango.update(mangoTest)
+        when:
+        def name1 = lookupCache.lookup(MangoTestEntity.class, mangoTest.getId(), MangoTestEntity.FIRSTNAME)
+        def name2 = lookupCache.lookup(MangoTestEntity.class, mangoTest.getId(), MangoTestEntity.FIRSTNAME)
+        def age = lookupCache.lookup(MangoTestEntity.class, mangoTest.getId(), MangoTestEntity.AGE)
+        def birthday = lookupCache.lookup(MangoTestEntity.class, mangoTest.getId(), MangoTestEntity.BIRTHDAY)
+        def cool = lookupCache.lookup(MangoTestEntity.class, mangoTest.getId(), MangoTestEntity.COOL)
+        def powers = lookupCache.lookup(MangoTestEntity.class, mangoTest.getId(), MangoTestEntity.SUPER_POWERS)
+        then:
+        name1 == "Gordon"
+        name2 == "Gordon"
+        age == 50
+        birthday == localDateTime
+        cool == true
+        powers == Arrays.asList("Cooking", "Raging")
     }
 
 }
