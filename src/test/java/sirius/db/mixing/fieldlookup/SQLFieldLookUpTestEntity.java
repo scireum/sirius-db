b91cce96e5df680dbd3f0672b04d9b3a82740c29
Marc Egenrieder <meg@scireum.de>
2020-07-24 10:58:07 +0200

    Extends FieldLookupCacheSpec to also test fields in mixins and composites

---
+++ src/test/java/sirius/db/mixing/fieldlookup/SQLFieldLookUpTestEntity.java
@@ -0,0 +1,63 @@
+/*
+ * Made with all the love in the world
+ * by scireum in Remshalden, Germany
+ *
+ * Copyright by scireum GmbH
+ * http://www.scireum.de - info@scireum.de
+ */
+
+package sirius.db.mixing.fieldlookup;
+
+import sirius.db.jdbc.SQLEntity;
+import sirius.db.mixing.Mapping;
+import sirius.db.mixing.annotations.NullAllowed;
+import sirius.db.mixing.types.StringList;
+
+import java.time.LocalDateTime;
+
+public class SQLFieldLookUpTestEntity extends SQLEntity {
+
+    public static final Mapping NAMES = Mapping.named("names");
+    private final NameFieldsTestComposite names = new NameFieldsTestComposite();
+
+    public static final Mapping AGE = Mapping.named("age");
+    private int age;
+
+    public static final Mapping COOL = Mapping.named("cool");
+    private boolean cool;
+
+    public static final Mapping BIRTHDAY = Mapping.named("birthday");
+    @NullAllowed
+    private LocalDateTime birthday;
+
+    public static final Mapping SUPER_POWERS = Mapping.named("superPowers");
+    private final StringList superPowers = new StringList();
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public boolean isCool() {
+        return cool;
+    }
+
+    public void setCool(boolean cool) {
+        this.cool = cool;
+    }
+
+    public LocalDateTime getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(LocalDateTime birthday) {
+        this.birthday = birthday;
+    }
+
+    public NameFieldsTestComposite getNames() {
+        return names;
+    }
+}
