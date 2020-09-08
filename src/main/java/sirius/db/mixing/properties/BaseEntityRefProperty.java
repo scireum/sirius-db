3e8094b4a58324e9e4b621ecab4325aece39ad0c
Florian Häfner <fha@scireum.de>
2020-09-08 14:02:59 +0200

    Show cascading deletions only to system administrators
    - Fixes: SIRI-250

--- src/main/java/sirius/db/mixing/properties/BaseEntityRefProperty.java
+++ src/main/java/sirius/db/mixing/properties/BaseEntityRefProperty.java
@@ -299,7 +299,7 @@ public abstract class BaseEntityRefProperty<I, E extends BaseEntity<I>, R extend
     private void cascadeDelete(TaskContext taskContext, BaseEntity<?> other) {
         Watch watch = Watch.start();
         other.getMapper().delete(other);
-        taskContext.addTiming(NLS.get("BaseEntityRefProperty.cascadedDelete"), watch.elapsedMillis());
+        taskContext.addTiming(NLS.get("BaseEntityRefProperty.cascadedDelete"), watch.elapsedMillis(), true);
     }
 
     protected void onDeleteReject(Object e) {
