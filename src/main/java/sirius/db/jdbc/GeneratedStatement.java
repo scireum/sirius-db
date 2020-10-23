0ca9e0dfe67ec645e1f49ffdb047e9069a79117e
aha <aha@scireum.de>
2020-10-23 10:21:03 +0200

    Fixes code style.

--- src/main/java/sirius/db/jdbc/GeneratedStatement.java
+++ src/main/java/sirius/db/jdbc/GeneratedStatement.java
@@ -202,7 +202,7 @@ abstract class GeneratedStatement<S extends GeneratedStatement<S>> {
                 return stmt.executeUpdate();
             }
         } finally {
-            watch.submitMicroTiming("SQL", microtimingKey()+": "+ sql);
+            watch.submitMicroTiming("SQL", microtimingKey() + ": " + sql);
         }
     }
 
