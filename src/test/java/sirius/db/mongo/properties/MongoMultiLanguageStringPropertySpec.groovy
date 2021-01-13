d32fd24559734110a6cbb9d52d7636d7f1d242a0
Idevaldo De Lira <ili@scireum.de>
2021-01-13 14:59:58 +0100

    Adapts test cases to cope with ignored blank strings
    Fixes: OX-6462

--- src/test/java/sirius/db/mongo/properties/MongoMultiLanguageStringPropertySpec.groovy
+++ src/test/java/sirius/db/mongo/properties/MongoMultiLanguageStringPropertySpec.groovy
@@ -48,7 +48,7 @@ class MongoMultiLanguageStringPropertySpec extends BaseSpecification {
     def "invalid language"() {
         given:
         def entity = new MongoMultiLanguageStringEntity()
-        entity.getMultiLangTextWithValidLanguages().addText("00", "")
+        entity.getMultiLangTextWithValidLanguages().addText("00", "some text")
 
         when:
         mango.update(entity)
@@ -60,7 +60,7 @@ class MongoMultiLanguageStringPropertySpec extends BaseSpecification {
     def "invalid language in composite"() {
         given:
         def entity = new MongoMultiLanguageStringEntity()
-        entity.getMultiLangComposite().getCompositeMultiLangTextWithValidLanguages().addText("00", "")
+        entity.getMultiLangComposite().getCompositeMultiLangTextWithValidLanguages().addText("00", "some text")
 
         when:
         mango.update(entity)
@@ -72,7 +72,7 @@ class MongoMultiLanguageStringPropertySpec extends BaseSpecification {
     def "invalid language in mixin"() {
         given:
         def entity = new MongoMultiLanguageStringEntityWithMixin()
-        entity.as(MongoMultiLanguageStringMixin.class).getMixinMultiLangTextWithValidLanguages().addText("00", "")
+        entity.as(MongoMultiLanguageStringMixin.class).getMixinMultiLangTextWithValidLanguages().addText("00", "some text")
 
         when:
         mango.update(entity)
@@ -158,7 +158,7 @@ class MongoMultiLanguageStringPropertySpec extends BaseSpecification {
         mango.update(entity)
 
         when:
-        def expectedString = "[Document{{lang=pt, text=Borboleta}}, Document{{lang=es, text=Mariposa}}, Document{{lang=en, text=}}]"
+        def expectedString = "[Document{{lang=pt, text=Borboleta}}, Document{{lang=es, text=Mariposa}}]"
         def storedString = mongo.find()
                                 .where("id", entity.getId())
                                 .singleIn("mongomultilanguagestringentity")
