faaa387970b69841442609ec05ba0a475295eff6
aha <aha@scireum.de>
2021-01-30 16:55:30 +0100

    Removes a duplicate check.
    A proper check has been installed by a separate branch.

--- src/main/java/sirius/db/mongo/Mango.java
+++ src/main/java/sirius/db/mongo/Mango.java
@@ -278,11 +278,6 @@ public class Mango extends BaseMapper<MongoEntity, MongoConstraint, MongoQuery<?
             return;
         }
 
-        if (!mongo.isConfigured()) {
-            Mongo.LOG.INFO("No configuration for MongoDB is present, skipping index creation...");
-            return;
-        }
-
         IntSummaryStatistics createdIndices = mixing.getDescriptors()
                                                     .stream()
                                                     .filter(descriptor -> MongoEntity.class.isAssignableFrom(descriptor.getType()))
