37dc05951716f5eea721df4846443df484034eb0
aha <aha@scireum.de>
2021-02-15 22:23:55 +0100

    Fixes sonarlint inspections.

--- src/main/java/sirius/db/mixing/Property.java
+++ src/main/java/sirius/db/mixing/Property.java
@@ -27,9 +27,6 @@ import javax.annotation.Nonnull;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
 
