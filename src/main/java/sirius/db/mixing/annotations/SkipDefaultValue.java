1e914647faab4a57fb1dd19c6b963496513e332c
aha <aha@scireum.de>
2021-02-15 22:19:41 +0100

    Permits to skip storing null or false values in mongo db.

---
+++ src/main/java/sirius/db/mixing/annotations/SkipDefaultValue.java
@@ -0,0 +1,27 @@
+/*
+ * Made with all the love in the world
+ * by scireum in Remshalden, Germany
+ *
+ * Copyright by scireum GmbH
+ * http://www.scireum.de - info@scireum.de
+ */
+
+package sirius.db.mixing.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Instructs the entity mapper to not store a value in the database if the current value is a default value.
+ * <p>
+ * This can e.g. be used for MongoDB where we can simply skip fields which are <tt>null</tt> or <tt>false</tt>, as when
+ * reading them back, the property will restore the default value anyway.
+ * <p>
+ * Note that this is not enabled by default as it migth lead to unexpected behavior in queries.
+ */
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface SkipDefaultValue {
+}
