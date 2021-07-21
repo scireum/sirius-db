586d7410b01e2e920604c5241fb3643d6712dc24
Marvin Woop <mwo@scireum.de>
2021-07-21 11:57:53 +0200

    Now throwing a more exact exception if multiple fields are used with count without using distinct. We internally discussed, that we want to treat a single field definition as count(*).

--- src/main/java/sirius/db/jdbc/SmartQuery.java
+++ src/main/java/sirius/db/jdbc/SmartQuery.java
@@ -808,11 +808,18 @@ public class SmartQuery<E extends SQLEntity> extends Query<SmartQuery<E>, E, SQL
         Compiler c = new Compiler(descriptor);
         c.getSELECTBuilder().append("SELECT COUNT(");
 
-        if (!fields.isEmpty() && distinct) {
-            c.getSELECTBuilder().append("DISTINCT ");
-            appendFieldList(c, false);
-        } else if (fields.stream().count() == 1) {
-            appendFieldList(c, false);
+        if (fields.size() > 1) {
+            if (distinct) {
+                c.getSELECTBuilder().append("DISTINCT ");
+                appendFieldList(c, false);
+            } else {
+                throw Exceptions.createHandled()
+                                .to(OMA.LOG)
+                                .withSystemErrorMessage(
+                                        "Only use multiple arguments in 'fields' and 'count' in "
+                                        + "combination with the 'distinct' statement")
+                                .handle();
+            }
         } else {
             c.getSELECTBuilder().append("*");
         }
