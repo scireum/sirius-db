c36325a850695dc68a7cdc10185bf92427f3f66e
Sascha Bieberstein <sbi@scireum.de>
2021-10-28 16:33:28 +0200

    Adds new annotations for upper/lower casing a string property

---
+++ src/main/java/sirius/db/mixing/annotations/UpperCase.java
@@ -0,0 +1,26 @@
+/*
+ * Made with all the love in the world
+ * by scireum in Remshalden, Germany
+ *
+ * Copyright by scireum GmbH
+ * http://www.scireum.de - info@scireum.de
+ */
+
+package sirius.db.mixing.annotations;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Marks a string property as auto upper-cased.
+ * <p>
+ * The value of this property will be upper-cased before it is written to the database.
+ */
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface UpperCase {
+}
