c5cff1189098476c3b06022e9ff97747d8c65ce4
Idevaldo De Lira <ili@scireum.de>
2023-06-14 13:53:35 +0200

    Don't cache values returned from secondary nodes

--- src/main/java/sirius/db/mixing/types/BaseEntityRef.java
+++ src/main/java/sirius/db/mixing/types/BaseEntityRef.java
@@ -169,18 +169,34 @@ public abstract class BaseEntityRef<I extends Serializable, E extends BaseEntity
      * @return the entity being referenced or <tt>null</tt> if no entity is referenced.
      */
     public E fetchValue() {
-        return fetchValue(false);
+        if (value == null && id != null) {
+            Optional<E> entity = find(type, id);
+            if (entity.isPresent()) {
+                value = entity.get();
+            } else {
+                id = null;
+            }
+        }
+        return value;
     }
 
     /**
      * Returns the effective entity object which is referenced using a secondary node of a DB cluster.
      * <p>
-     * Note, this might cause a database lookup if the entity is not prefetched.
+     * Note, this values returned by this method will never be cached and a new lookup is always performed.
      *
      * @return the entity being referenced or <tt>null</tt> if no entity is referenced.
      */
     public E fetchValueFromSecondary() {
-        return fetchValue(true);
+        if (value == null && id != null) {
+            Optional<E> entity = findInSecondary(type, id);
+            if (entity.isPresent()) {
+                return entity.get();
+            } else {
+                id = null;
+            }
+        }
+        return value;
     }
 
     /**
@@ -304,17 +320,5 @@ public abstract class BaseEntityRef<I extends Serializable, E extends BaseEntity
         } else {
             return "(empty)";
         }
-    }
-
-    private E fetchValue(boolean useSecondary) {
-        if (value == null && id != null) {
-            Optional<E> entity = useSecondary ? findInSecondary(type, id) : find(type, id);
-            if (entity.isPresent()) {
-                value = entity.get();
-            } else {
-                id = null;
-            }
-        }
-        return value;
     }
 }
