967c4c25fc3f0b10f6222dfd7239f12f475e4ae5
Mauricio Muler Bodemann <mbo@scireum.de>
2024-01-26 16:35:07 +0100

    Adapts to kotlin syntax
    Fixes: OX-10357

--- src/test/kotlin/sirius/db/mongo/MongoQueryCompilerTest.kt
+++ src/test/kotlin/sirius/db/mongo/MongoQueryCompilerTest.kt
@@ -8,40 +8,56 @@
 
 package sirius.db.mongo
 
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
 import sirius.db.mixing.Mixing
-import sirius.kernel.BaseSpecification
+import sirius.kernel.SiriusExtension
 import sirius.kernel.di.std.Part
+import kotlin.test.assertEquals
+import kotlin.test.assertFalse
+import kotlin.test.assertNotNull
 
-class MongoQueryCompilerSpec extends BaseSpecification {
+@ExtendWith(SiriusExtension::class)
+class MongoQueryCompilerTest {
+    companion object {
+        @Part
+        private lateinit var mango: Mango
 
-    @Part
-    private static Mango mango
-
-            @Part
-            private static Mixing mixing
-
-            def "listField:- generates an isEmptyListConstraint"() {
-        when: "We create an example entity with an empty list (superPowers)"
-        MangoTestEntity testEntity = new MangoTestEntity()
-        testEntity.firstname = "Compiler"
-        testEntity.lastname = "Test"
-        and:
-        mango.update(testEntity)
-        and: "And we search for exactly that entity while enforcing an empty list."
-        MangoTestEntity queryResult = mango.
-        select(MangoTestEntity.class).
-        where(QueryBuilder.FILTERS.queryString(mixing.getDescriptor(MangoTestEntity.class),
-                "id:${testEntity.getIdAsString()} superPowers:-")).
-        queryFirst()
-                then: "This entity is found..."
-                queryResult != null
-                queryResult.getId() == testEntity.getId()
-                and: "And filtering on a real value within the list still also works..."
-                !mango.
-                select(MangoTestEntity.class).
-                where(QueryBuilder.FILTERS.queryString(mixing.getDescriptor(MangoTestEntity.class),
-                        "id:${testEntity.getIdAsString()} superPowers:WriteCode")).
-                exists()
+        @Part
+        private lateinit var mixing: Mixing
     }
 
+    @Test
+    fun `listField generates an isEmptyListConstraint`() {
+        val testEntity = MangoTestEntity()
+        testEntity.firstname = "Compiler"
+        testEntity.lastname = "Test"
+        mango.update(testEntity)
+
+        val queryResult = mango.select(
+                MangoTestEntity::class.java
+        ).where(
+                QueryBuilder.FILTERS.queryString(
+                        mixing.getDescriptor(
+                                MangoTestEntity::class.java
+                        ),
+                        "id:${testEntity.getIdAsString()} superPowers:-"
+                )
+        ).queryFirst()
+
+        assertNotNull(queryResult)
+        assertEquals(testEntity.id, queryResult.id)
+        assertFalse {
+            mango.select(
+                    MangoTestEntity::class.java
+            ).where(
+                    QueryBuilder.FILTERS.queryString(
+                            mixing.getDescriptor(
+                                    MangoTestEntity::class.java
+                            ),
+                            "id:${testEntity.getIdAsString()} superPowers:WriteCode"
+                    )
+            ).exists()
+        }
+    }
 }
