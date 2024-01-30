348742525beab444303176e694a50de214bb8158
Mauricio Muler Bodemann <mbo@scireum.de>
2024-01-30 15:11:12 +0100

    Moves CompositeTest to new file
    Fixes: OX-10357

---
+++ src/test/kotlin/sirius/db/mixing/CompositeTest.kt
@@ -0,0 +1,31 @@
+/*
+ * Made with all the love in the world
+ * by scireum in Remshalden, Germany
+ *
+ * Copyright by scireum GmbH
+ * http://www.scireum.de - info@scireum.de
+ */
+
+package sirius.db.mixing
+
+
+import sirius.db.mongo.Mango
+import sirius.kernel.BaseSpecification
+import sirius.kernel.di.std.Part
+import sirius.kernel.health.HandledException
+
+class CompositeSpec extends BaseSpecification {
+
+    @Part
+    private static Mango mango
+
+            def "Updating Composite inside Mixable works on Mongo"() {
+        when:
+        RefMongoEntity refMongoEntity = new RefMongoEntity()
+        refMongoEntity.as(MongoMixable.class).getComposite().getMap().modify().put("test", "data")
+                mango.update(refMongoEntity)
+                then:
+                notThrown(HandledException)
+    }
+
+}
