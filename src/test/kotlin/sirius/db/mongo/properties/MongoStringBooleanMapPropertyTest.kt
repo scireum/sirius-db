1d411922df61626c0b363b84be858b3729f3c63e
Mauricio Muler Bodemann <mbo@scireum.de>
2024-01-30 13:40:50 +0100

    Adapts to kotlin syntax
    Fixes: OX-10357

--- src/test/kotlin/sirius/db/mongo/properties/MongoStringBooleanMapPropertyTest.kt
+++ src/test/kotlin/sirius/db/mongo/properties/MongoStringBooleanMapPropertyTest.kt
@@ -8,38 +8,41 @@
 
 package sirius.db.mongo.properties
 
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
 import sirius.db.mongo.Mango
-import sirius.kernel.BaseSpecification
+import sirius.kernel.SiriusExtension
 import sirius.kernel.di.std.Part
+import kotlin.test.assertEquals
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
 
-class MongoStringBooleanMapPropertySpec extends BaseSpecification {
+@ExtendWith(SiriusExtension::class)
+class MongoStringBooleanMapPropertyTest {
+    @Test
+    fun `read and write a string boolean map works`() {
+        val mongoStringBooleanMapEntity = MongoStringBooleanMapEntity()
+        mongoStringBooleanMapEntity.map.put("a", true).put("b", false).put("c", true).put("d", false)
+        mango.update(mongoStringBooleanMapEntity)
+        var resolved = mango.refreshOrFail(mongoStringBooleanMapEntity)
 
-    @Part
-    private static Mango mango
+        assertEquals(4, resolved.map.size())
+        assertTrue { resolved.map.get("a").get() }
+        assertFalse { resolved.map.get("b").get() }
+        assertTrue { resolved.map.get("c").get() }
+        assertFalse { resolved.map.get("d").get() }
 
-            def "read/write a string boolean map works"() {
-        when:
-        def test = new MongoStringBooleanMapEntity()
-        test.getMap().put("a", true).put("b", false).put("c", new Boolean(true)).put("d", new Boolean(false))
-        mango.update(test)
-        def resolved = mango.refreshOrFail(test)
-        then:
-        resolved.getMap().size() == 4
-        and:
-        resolved.getMap().get("a").get()
-        !resolved.getMap().get("b").get()
-        resolved.getMap().get("c").get()
-        !resolved.getMap().get("d").get()
-        when:
-        resolved.getMap().modify().remove("a")
-        and:
+        resolved.map.modify().remove("a")
         mango.update(resolved)
-        and:
-        resolved = mango.refreshOrFail(test)
-        then:
-        resolved.getMap().size() == 3
-        and:
-        !resolved.getMap().containsKey("a")
-        resolved.getMap().get("c").get()
+        resolved = mango.refreshOrFail(mongoStringBooleanMapEntity)
+
+        assertEquals(3, resolved.map.size())
+        assertFalse { resolved.map.containsKey("a") }
+        assertTrue { resolved.map.get("c").get() }
+    }
+
+    companion object {
+        @Part
+        private lateinit var mango: Mango
     }
 }
