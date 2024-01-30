402fc0dfb903628413f9e69482c60a3c5f1c4aa4
Mauricio Muler Bodemann <mbo@scireum.de>
2024-01-30 15:14:14 +0100

    Adapts to kotlin syntax
    Fixes: OX-10357

--- src/test/kotlin/sirius/db/mixing/CompositeTest.kt
+++ src/test/kotlin/sirius/db/mixing/CompositeTest.kt
@@ -8,24 +8,25 @@
 
 package sirius.db.mixing
 
-
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
+import org.junit.jupiter.api.extension.ExtendWith
 import sirius.db.mongo.Mango
-import sirius.kernel.BaseSpecification
+import sirius.kernel.SiriusExtension
 import sirius.kernel.di.std.Part
-import sirius.kernel.health.HandledException
 
-class CompositeSpec extends BaseSpecification {
+@ExtendWith(SiriusExtension::class)
+class CompositeTest {
+    @Test
+    fun `Updating Composite inside Mixable works on Mongo`() {
+        val refMongoEntity = RefMongoEntity()
+        refMongoEntity.`as`(MongoMixable::class.java).composite.map.modify()["test"] = "data"
 
-    @Part
-    private static Mango mango
-
-            def "Updating Composite inside Mixable works on Mongo"() {
-        when:
-        RefMongoEntity refMongoEntity = new RefMongoEntity()
-        refMongoEntity.as(MongoMixable.class).getComposite().getMap().modify().put("test", "data")
-                mango.update(refMongoEntity)
-                then:
-                notThrown(HandledException)
+        assertDoesNotThrow { mango.update(refMongoEntity) }
     }
 
+    companion object {
+        @Part
+        private lateinit var mango: Mango
+    }
 }
