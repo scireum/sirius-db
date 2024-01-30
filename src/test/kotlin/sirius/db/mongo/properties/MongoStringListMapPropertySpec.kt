3af36b837f557e7e107198996246f687cbd72da1
Mauricio Muler Bodemann <mbo@scireum.de>
2024-01-30 13:52:35 +0100

    Adapts to kotlin syntax
    Fixes: OX-10357

--- src/test/kotlin/sirius/db/mongo/properties/MongoStringListMapPropertySpec.kt
+++ src/test/kotlin/sirius/db/mongo/properties/MongoStringListMapPropertySpec.kt
@@ -8,44 +8,47 @@
 
 package sirius.db.mongo.properties
 
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
 import sirius.db.mongo.Mango
 import sirius.db.mongo.Mongo
-import sirius.kernel.BaseSpecification
+import sirius.kernel.SiriusExtension
 import sirius.kernel.di.std.Part
+import kotlin.test.assertEquals
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
 
-class MongoStringListMapPropertySpec extends BaseSpecification {
+@ExtendWith(SiriusExtension::class)
+class MongoStringListMapPropertyTest {
+    @Test
+    fun `reading and writing works`() {
+        val mongoStringListMapEntity = MongoStringListMapEntity()
+        mongoStringListMapEntity.map.add("Test", "1").add("Foo", "2").add("Test", "3")
+        mango.update(mongoStringListMapEntity)
+        var resolved = mango.refreshOrFail(mongoStringListMapEntity)
 
-    @Part
-    private static Mango mango
+        assertEquals(2, resolved.map.size())
 
-            @Part
-            private static Mongo mongo
+        assertTrue { resolved.map.contains("Test", "1") }
+        assertTrue { resolved.map.contains("Test", "3") }
+        assertTrue { resolved.map.contains("Foo", "2") }
 
-            def "reading and writing works"() {
-        when:
-        def test = new MongoStringListMapEntity()
-        test.getMap().add("Test", "1").add("Foo", "2").add("Test", "3")
-        mango.update(test)
-        def resolved = mango.refreshOrFail(test)
-        then:
-        resolved.getMap().size() == 2
-        and:
-        resolved.getMap().contains("Test", "1")
-        resolved.getMap().contains("Test", "3")
-        resolved.getMap().contains("Foo", "2")
-
-        when:
-        resolved.getMap().remove("Test", "1")
-        and:
+        resolved.map.remove("Test", "1")
         mango.update(resolved)
-        and:
-        resolved = mango.refreshOrFail(test)
-        then:
-        resolved.getMap().size() == 2
-        and:
-        !resolved.getMap().contains("Test", "1")
-        resolved.getMap().contains("Test", "3")
-        resolved.getMap().contains("Foo", "2")
+        resolved = mango.refreshOrFail(mongoStringListMapEntity)
+
+        assertEquals(2, resolved.map.size())
+
+        assertFalse { resolved.map.contains("Test", "1") }
+        assertTrue { resolved.map.contains("Test", "3") }
+        assertTrue { resolved.map.contains("Foo", "2") }
     }
 
+    companion object {
+        @Part
+        private lateinit var mango: Mango
+
+        @Part
+        private lateinit var mongo: Mongo
+    }
 }
