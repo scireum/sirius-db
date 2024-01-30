02506a107e6ec7b1bcd4d2676490d294015b25a9
Mauricio Muler Bodemann <mbo@scireum.de>
2024-01-30 13:21:25 +0100

    Adapts to kotlin syntax
    Fixes: OX-10357

--- src/test/kotlin/sirius/db/mongo/properties/MongoNestedListPropertyTest.kt
+++ src/test/kotlin/sirius/db/mongo/properties/MongoNestedListPropertyTest.kt
@@ -8,51 +8,47 @@
 
 package sirius.db.mongo.properties
 
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
 import sirius.db.mongo.Mango
 import sirius.db.mongo.Mongo
-import sirius.kernel.BaseSpecification
+import sirius.kernel.SiriusExtension
 import sirius.kernel.di.std.Part
+import kotlin.test.assertEquals
 
-class MongoNestedListPropertySpec extends BaseSpecification {
-
-    @Part
-    private static Mango mango
-
-            @Part
-            private static Mongo mongo
-
-            def "reading, change tracking and writing works"() {
-        when:
-        def test = new MongoNestedListEntity()
-        test.getList().add(new MongoNestedListEntity.NestedEntity().withValue1("X").withValue2("Y"))
+@ExtendWith(SiriusExtension::class)
+class MongoNestedListPropertyTest {
+    @Test
+    fun `reading, change tracking and writing works`() {
+        val test = MongoNestedListEntity()
+        test.list.add(MongoNestedListEntity.NestedEntity().withValue1("X").withValue2("Y"))
         mango.update(test)
-        def resolved = mango.refreshOrFail(test)
-        then:
-        resolved.getList().size() == 1
-        and:
-        resolved.getList().data().get(0).getValue1() == "X"
-        resolved.getList().data().get(0).getValue2() == "Y"
+        var resolved = mango.refreshOrFail(test)
 
-        when:
-        resolved.getList().modify().get(0).withValue1("Z")
-        and:
-        mango.update(resolved)
-        and:
-        resolved = mango.refreshOrFail(test)
-        then:
-        resolved.getList().size() == 1
-        and:
-        resolved.getList().data().get(0).getValue1() == "Z"
-        resolved.getList().data().get(0).getValue2() == "Y"
+        assertEquals(1, resolved.list.size())
+        assertEquals("X", resolved.list.data()[0].value1)
+        assertEquals("Y", resolved.list.data()[0].value2)
 
-        when:
-        resolved.getList().modify().remove(0)
-        and:
+        resolved.list.modify().get(0).withValue1("Z")
         mango.update(resolved)
-        and:
         resolved = mango.refreshOrFail(test)
-        then:
-        resolved.getList().size() == 0
+
+        assertEquals(1, resolved.list.size())
+        assertEquals("Z", resolved.list.data()[0].value1)
+        assertEquals("Y", resolved.list.data()[0].value2)
+
+        resolved.list.modify().removeAt(0)
+        mango.update(resolved)
+        resolved = mango.refreshOrFail(test)
+
+        assertEquals(0, resolved.list.size())
     }
 
+    companion object {
+        @Part
+        private lateinit var mango: Mango
+
+        @Part
+        private lateinit var mongo: Mongo
+    }
 }
