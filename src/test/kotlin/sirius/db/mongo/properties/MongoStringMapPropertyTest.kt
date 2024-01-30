1b877a2b22505481a9d3e3c1ac079009cc732693
Mauricio Muler Bodemann <mbo@scireum.de>
2024-01-30 14:26:26 +0100

    Moves MongoStringMapPropertyTest to new file
    Fixes: OX-10357

---
+++ src/test/kotlin/sirius/db/mongo/properties/MongoStringMapPropertyTest.kt
@@ -0,0 +1,49 @@
+/*
+ * Made with all the love in the world
+ * by scireum in Remshalden, Germany
+ *
+ * Copyright by scireum GmbH
+ * http://www.scireum.de - info@scireum.de
+ */
+
+package sirius.db.mongo.properties
+
+import sirius.db.mongo.Mango
+import sirius.db.mongo.Mongo
+import sirius.kernel.BaseSpecification
+import sirius.kernel.di.std.Part
+
+class MongoStringMapPropertySpec extends BaseSpecification {
+
+    @Part
+    private static Mango mango
+
+            @Part
+            private static Mongo mongo
+
+            def "reading and writing works"() {
+        when:
+        def test = new MongoStringMapEntity()
+        test.getMap().put("Test", "1").put("Foo", "2")
+        mango.update(test)
+        def resolved = mango.refreshOrFail(test)
+        then:
+        resolved.getMap().size() == 2
+        and:
+        resolved.getMap().get("Test").get() == "1"
+        resolved.getMap().get("Foo").get() == "2"
+
+        when:
+        resolved.getMap().modify().remove("Test")
+        and:
+        mango.update(resolved)
+        and:
+        resolved = mango.refreshOrFail(test)
+        then:
+        resolved.getMap().size() == 1
+        and:
+        !resolved.getMap().contains("Test")
+        resolved.getMap().get("Foo").get() == "2"
+    }
+
+}
