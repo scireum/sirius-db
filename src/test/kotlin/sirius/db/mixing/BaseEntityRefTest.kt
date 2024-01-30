bc0fb46803cb3348bca82c93718b857a6530bc05
Mauricio Muler Bodemann <mbo@scireum.de>
2024-01-30 15:10:12 +0100

    Adapts to kotlin syntax
    Fixes: OX-10357

--- src/test/kotlin/sirius/db/mixing/BaseEntityRefTest.kt
+++ src/test/kotlin/sirius/db/mixing/BaseEntityRefTest.kt
@@ -8,111 +8,111 @@
 
 package sirius.db.mixing
 
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.api.extension.ExtendWith
 import sirius.db.es.Elastic
 import sirius.db.jdbc.OMA
 import sirius.db.mongo.Mango
-import sirius.kernel.BaseSpecification
-import sirius.kernel.commons.Wait
+import sirius.kernel.SiriusExtension
 import sirius.kernel.di.std.Part
 import sirius.kernel.health.HandledException
-
 import java.time.Duration
+import kotlin.test.assertFalse
 
-class BaseEntityRefSpec extends BaseSpecification {
-
-    @Part
-    private static Elastic elastic
-
-            @Part
-            private static OMA oma
-
-            @Part
-            private static Mango mango
-
-            def setupSpec() {
-        elastic.getReadyFuture().await(Duration.ofSeconds(60))
-        oma.getReadyFuture().await(Duration.ofSeconds(60))
-    }
-
-    def "cascade from JDBC to ES and Mongo works"() {
-        when:
-        RefEntity refEntity = new RefEntity()
+@ExtendWith(SiriusExtension::class)
+class BaseEntityRefTest {
+    @Test
+    fun `cascade from JDBC to ES and Mongo works`() {
+        val refEntity = RefEntity()
         oma.update(refEntity)
-
-        RefElasticEntity refElasticEntity = new RefElasticEntity()
-        refElasticEntity.getRef().setValue(refEntity)
+        val refElasticEntity = RefElasticEntity()
+        refElasticEntity.ref.setValue(refEntity)
         elastic.update(refElasticEntity)
-        elastic.refresh(RefElasticEntity.class)
-
-                RefMongoEntity refMongoEntity = new RefMongoEntity()
-                refMongoEntity.getRef().setValue(refEntity)
-                mango.update(refMongoEntity)
-                and:
-                oma.delete(refEntity)
-                elastic.refresh(RefElasticEntity.class)
-                then:
-                !elastic.find(RefElasticEntity.class, refElasticEntity.getId()).isPresent()
-                        and:
-                !mango.find(RefMongoEntity.class, refMongoEntity.getId()).isPresent()
-    }
-
-    def "cascade from ES to JDBC works"() {
-        when:
-        RefElasticEntity refElasticEntity = new RefElasticEntity()
-        elastic.update(refElasticEntity)
-        RefEntity refEntity = new RefEntity()
-        refEntity.getElastic().setValue(refElasticEntity)
-        oma.update(refEntity)
-        and:
-        elastic.delete(refElasticEntity)
-        then:
-        !oma.find(RefEntity.class, refEntity.getId()).isPresent()
-    }
-
-    def "cascade from Mongo to JDBC works"() {
-        when:
-        RefMongoEntity refMongoEntity = new RefMongoEntity()
+        elastic.refresh(RefElasticEntity::class.java)
+        val refMongoEntity = RefMongoEntity()
+        refMongoEntity.ref.setValue(refEntity)
         mango.update(refMongoEntity)
-        RefEntity refEntity = new RefEntity()
-        refEntity.getMongo().setValue(refMongoEntity)
+        oma.delete(refEntity)
+        elastic.refresh(RefElasticEntity::class.java)
+
+        assertFalse { elastic.find(RefElasticEntity::class.java, refElasticEntity.id).isPresent }
+        assertFalse { mango.find(RefMongoEntity::class.java, refMongoEntity.id).isPresent }
+    }
+
+    @Test
+    fun `cascade from ES to JDBC works`() {
+        val refElasticEntity = RefElasticEntity()
+        elastic.update(refElasticEntity)
+        val refEntity = RefEntity()
+        refEntity.elastic.setValue(refElasticEntity)
         oma.update(refEntity)
-        and:
+        elastic.delete(refElasticEntity)
+
+        assertFalse { oma.find(RefEntity::class.java, refEntity.id).isPresent }
+    }
+
+    @Test
+    fun `cascade from Mongo to JDBC works`() {
+        val refMongoEntity = RefMongoEntity()
+        mango.update(refMongoEntity)
+
+        val refEntity = RefEntity()
+        refEntity.mongo.setValue(refMongoEntity)
+        oma.update(refEntity)
+
         mango.delete(refMongoEntity)
-        then:
-        !oma.find(RefEntity.class, refEntity.getId()).isPresent()
+
+        assertFalse { oma.find(RefEntity::class.java, refEntity.id).isPresent }
     }
 
-    def "writeOnce semantics are enforced"() {
-        when:
-        WriteOnceParentEntity parent = new WriteOnceParentEntity()
-        oma.update(parent)
-        and:
-        WriteOnceChildEntity child = new WriteOnceChildEntity()
-        child.getParent().setValue(parent)
-        oma.update(child)
-        and:
-        WriteOnceParentEntity anotherParent = new WriteOnceParentEntity()
-        oma.update(anotherParent)
-        and:
-        child.getParent().setValue(anotherParent)
-        oma.update(child)
-        then:
-        thrown(HandledException)
+    @Test
+    fun `writeOnce semantics are enforced`() {
+        assertThrows<HandledException> {
+            val writeOnceParentEntity1 = WriteOnceParentEntity()
+            oma.update(writeOnceParentEntity1)
+
+            val writeOnceChildEntity = WriteOnceChildEntity()
+            writeOnceChildEntity.parent.setValue(writeOnceParentEntity1)
+            oma.update(writeOnceChildEntity)
+
+            val writeOnceParentEntity2 = WriteOnceParentEntity()
+            oma.update(writeOnceParentEntity2)
+
+            writeOnceChildEntity.parent.setValue(writeOnceParentEntity2)
+            oma.update(writeOnceChildEntity)
+        }
     }
 
-    def "writeOnce semantics permit a non-changing update"() {
-        when:
-        WriteOnceParentEntity parent = new WriteOnceParentEntity()
-        oma.update(parent)
-        and:
-        WriteOnceChildEntity child = new WriteOnceChildEntity()
-        child.getParent().setValue(parent)
-        oma.update(child)
-        and:
-        child.getParent().setValue(parent)
-        oma.update(child)
-        then:
-        notThrown(HandledException)
+    @Test
+    fun `writeOnce semantics permit a non-changing update`() {
+        assertDoesNotThrow {
+            val writeOnceParentEntity = WriteOnceParentEntity()
+            oma.update(writeOnceParentEntity)
+
+            val writeOnceChildEntity = WriteOnceChildEntity()
+            writeOnceChildEntity.parent.setValue(writeOnceParentEntity)
+            oma.update(writeOnceChildEntity)
+
+            writeOnceChildEntity.parent.setValue(writeOnceParentEntity)
+            oma.update(writeOnceChildEntity)
+        }
     }
 
+    companion object {
+        @Part
+        private lateinit var elastic: Elastic
+
+        @Part
+        private lateinit var oma: OMA
+
+        @Part
+        private lateinit var mango: Mango
+
+        fun setupSpec() {
+            elastic.readyFuture.await(Duration.ofSeconds(60))
+            oma.readyFuture.await(Duration.ofSeconds(60))
+        }
+    }
 }
