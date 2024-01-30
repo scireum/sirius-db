09ded8d0f2fb681ad46ff3f199f6e6482652cd6d
Mauricio Muler Bodemann <mbo@scireum.de>
2024-01-30 14:05:26 +0100

    Adapts to kotlin syntax
    Fixes: OX-10357

--- src/test/kotlin/sirius/db/mongo/properties/MongoStringListMixinEntityTest.kt
+++ src/test/kotlin/sirius/db/mongo/properties/MongoStringListMixinEntityTest.kt
@@ -8,33 +8,37 @@
 
 package sirius.db.mongo.properties
 
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
 import sirius.db.mongo.Mango
-import sirius.kernel.BaseSpecification
+import sirius.kernel.SiriusExtension
 import sirius.kernel.di.std.Part
+import kotlin.test.assertEquals
 
-class MongoStringListMixinEntitySpec extends BaseSpecification {
+@ExtendWith(SiriusExtension::class)
+class MongoStringListMixinEntityTest {
+    @Test
+    fun `reading and writing works`() {
+        val mongoStringListMixinEntity = MongoStringListMixinEntity()
+        mongoStringListMixinEntity.`as`(MongoStringListMixin::class.java).listInMixin.add("hello").add("world")
+        mango.update(mongoStringListMixinEntity)
+        val refreshed = mango.refreshOrFail(mongoStringListMixinEntity)
+        val mixinOfRefreshed = refreshed.`as`(MongoStringListMixin::class.java)
 
-    @Part
-    private static Mango mango
+        assertEquals(2, mixinOfRefreshed.listInMixin.size())
+        assertEquals("hello", mixinOfRefreshed.listInMixin.data()[0])
+        assertEquals("world", mixinOfRefreshed.listInMixin.data()[1])
 
-            def "reading and writing works"() {
-        when:
-        def test = new MongoStringListMixinEntity()
-        test.as(MongoStringListMixin.class).getListInMixin().add("hello").add("world")
-                mango.update(test)
-                def refreshed = mango.refreshOrFail(test)
-                then:
-                def mixinOfRefreshed = refreshed.as(MongoStringListMixin.class)
-                mixinOfRefreshed.getListInMixin().size() == 2
-                mixinOfRefreshed.getListInMixin().data().get(0) == "hello"
-                mixinOfRefreshed.getListInMixin().data().get(1) == "world"
+        val queried = mango.find(MongoStringListMixinEntity::class.java, mongoStringListMixinEntity.id).get()
+        val mixinOfQueried = queried.`as`(MongoStringListMixin::class.java)
 
-                when:
-        def queried = mango.find(MongoStringListMixinEntity.class, test.getId()).get()
-                then:
-                def mixinOfQueried = queried.as(MongoStringListMixin.class)
-                mixinOfQueried.getListInMixin().size() == 2
-                mixinOfQueried.getListInMixin().data().get(0) == "hello"
-                mixinOfQueried.getListInMixin().data().get(1) == "world"
+        assertEquals(2, mixinOfQueried.listInMixin.size())
+        assertEquals("hello", mixinOfQueried.listInMixin.data()[0])
+        assertEquals("world", mixinOfQueried.listInMixin.data()[1])
+    }
+
+    companion object {
+        @Part
+        private lateinit var mango: Mango
     }
 }
