d4bb3fbd2de2de1f5ee5532e87d7ded82abbad22
Mauricio Muler Bodemann <mbo@scireum.de>
2024-01-30 16:06:48 +0100

    Adapts to kotlin syntax
    Fixes: OX-10357

--- src/test/kotlin/sirius/db/mixing/properties/DefaultValuesTest.kt
+++ src/test/kotlin/sirius/db/mixing/properties/DefaultValuesTest.kt
@@ -8,147 +8,172 @@
 
 package sirius.db.mixing.properties
 
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
 import sirius.db.jdbc.DataTypesEntity
 import sirius.db.mixing.Mixing
 import sirius.db.mixing.SQLDefaultValuesEntity
-import sirius.kernel.BaseSpecification
+import sirius.kernel.SiriusExtension
 import sirius.kernel.commons.Amount
 import sirius.kernel.commons.Value
 import sirius.kernel.di.std.Part
+import java.util.stream.Stream
+import kotlin.test.assertEquals
+import kotlin.test.assertFalse
+import kotlin.test.assertNull
+import kotlin.test.assertTrue
 
-class DefaultValuesSpec extends BaseSpecification {
-
-    @Part
-    private static Mixing mixing
-
-            def "the default values are properly initialized"() {
-        expect:
-        mixing.getDescriptor(SQLDefaultValuesEntity.class).findProperty(propertyName).getDefaultValue() ==
-                expectedDefault
-
-                where:
-                propertyName            | expectedDefault
-                "primitiveBoolean"      | Value.of(false)
-        "primitiveBooleanTrue"  | Value.of(true)
-        "booleanObject"         | Value.EMPTY
-        "primitiveInt"          | Value.of(0)
-        "primitiveIntWithValue" | Value.of(50)
-        "amountWithValue"       | Value.of(Amount.ONE_HUNDRED)
-        "amountZero"            | Value.of(Amount.ZERO)
-        "amountNothing"         | Value.EMPTY //Amount.NOTHING should be considered null and therefore have no default
-        "string"                | Value.EMPTY
-        "emptyString"           | Value.of("")
-        "enumWithValue"         | Value.of(DataTypesEntity.TestEnum.Test2)
+@ExtendWith(SiriusExtension::class)
+class DefaultValuesTest {
+    @ParameterizedTest
+    @MethodSource("provideParametersForValuesInitialized")
+    fun `the default values are properly initialized`(propertyName: String, expectedDefaultValue: Value) {
+        assertEquals(
+                expectedDefaultValue,
+                mixing.getDescriptor(SQLDefaultValuesEntity::class.java).findProperty(propertyName)?.defaultValue
+        )
     }
 
-    def "column default values are properly transformed"() {
-        expect:
-        mixing.getDescriptor(SQLDefaultValuesEntity.class).findProperty(propertyName).getColumnDefaultValue() ==
-                expectedDefault
-
-                where:
-                propertyName            | expectedDefault
-                "primitiveBoolean"      | "0"
-        "primitiveBooleanTrue"  | "1"
-        "booleanObject"         | null
-        "primitiveInt"          | "0"
-        "primitiveIntWithValue" | "50"
-        "amountWithValue"       | "100.000"
-        "amountZero"            | "0.000"
-        "amountNothing"         | null
-        "string"                | null
-        "emptyString"           | ""
-        "filledString"          | "STRING"
-        "enumTest"              | null
-        "enumWithValue"         | "Test2"
+    @ParameterizedTest
+    @MethodSource("provideParametersForValuesTransformed")
+    fun `column default values are properly transformed`(propertyName: String, expectedDefault: String?) {
+        assertEquals(
+                expectedDefault,
+                mixing.getDescriptor(SQLDefaultValuesEntity::class.java).findProperty(propertyName)
+                        ?.getColumnDefaultValue()
+        )
     }
 
-    def "a primitive boolean with no default value annotation does not throw an error"() {
-        given:
-        def property = mixing.getDescriptor(SQLDefaultValuesEntity.class).findProperty("primitiveBoolean")
-                and:
-                SQLDefaultValuesEntity entity = new SQLDefaultValuesEntity()
-                entity.setPrimitiveBoolean(true)
-        when: // an empty value is given, its default (false) should be applied
-        property.parseValueFromImport(entity, Value.EMPTY)
-        then:
-        !entity.isPrimitiveBoolean()
+    @Test
+    fun `a primitive boolean with no default value annotation does not throw an error`() {
+        val property = mixing.getDescriptor(SQLDefaultValuesEntity::class.java).findProperty("primitiveBoolean")
+        val sqlDefaultValuesEntity = SQLDefaultValuesEntity()
+        sqlDefaultValuesEntity.isPrimitiveBoolean = true
+        property?.parseValueFromImport(sqlDefaultValuesEntity, Value.EMPTY)
+
+        assertFalse { sqlDefaultValuesEntity.isPrimitiveBoolean }
     }
 
-    def "a primitive boolean gets its default value from its initial assigned value"() {
-        given:
-        def property = mixing.getDescriptor(SQLDefaultValuesEntity.class).findProperty("primitiveBooleanTrue")
-                and:
-                SQLDefaultValuesEntity entity = new SQLDefaultValuesEntity()
-        when: // an empty value is given, its default (true) should be applied
-        property.parseValueFromImport(entity, Value.EMPTY)
-        then:
-        entity.isPrimitiveBooleanTrue()
+    @Test
+    fun `a primitive boolean gets its default value from its initial assigned value`() {
+        val property = mixing.getDescriptor(SQLDefaultValuesEntity::class.java).findProperty("primitiveBooleanTrue")
+        val sqlDefaultValuesEntity = SQLDefaultValuesEntity()
+        property?.parseValueFromImport(sqlDefaultValuesEntity, Value.EMPTY)
+
+        assertTrue { sqlDefaultValuesEntity.isPrimitiveBooleanTrue }
     }
 
-    def "a boolean object field can have no default value"() {
-        given:
-        def property = mixing.getDescriptor(SQLDefaultValuesEntity.class).findProperty("booleanObject")
-                and:
-                SQLDefaultValuesEntity entity = new SQLDefaultValuesEntity()
-                entity.setBooleanObject(Boolean.TRUE)
-        when: // an empty value is given, the field should reset to 'null'
-        property.parseValueFromImport(entity, Value.EMPTY)
-        then:
-        entity.getBooleanObject() == null
+    @Test
+    fun `a boolean object field can have no default value`() {
+        val property = mixing.getDescriptor(SQLDefaultValuesEntity::class.java).findProperty("booleanObject")
+        val sqlDefaultValuesEntity = SQLDefaultValuesEntity()
+        sqlDefaultValuesEntity.booleanObject = true
+        property?.parseValueFromImport(sqlDefaultValuesEntity, Value.EMPTY)
+
+        assertNull(sqlDefaultValuesEntity.booleanObject)
     }
 
-    def "primitive number fields get automatic default value from their initial default value"() {
-        given:
-        def property = mixing.getDescriptor(SQLDefaultValuesEntity.class).findProperty("primitiveInt")
-                and:
-                SQLDefaultValuesEntity entity = new SQLDefaultValuesEntity()
-                entity.setPrimitiveInt(12)
-        when: // an empty value is given, its default (0) should be applied
-        property.parseValueFromImport(entity, Value.EMPTY)
-        then:
-        entity.getPrimitiveInt() == 0
+    @Test
+    fun `primitive number fields get automatic default value from their initial default value`() {
+        val property = mixing.getDescriptor(SQLDefaultValuesEntity::class.java).findProperty("primitiveInt")
+        val sqlDefaultValuesEntity = SQLDefaultValuesEntity()
+        sqlDefaultValuesEntity.primitiveInt = 12
+        property?.parseValueFromImport(sqlDefaultValuesEntity, Value.EMPTY)
+
+        assertEquals(0, sqlDefaultValuesEntity.primitiveInt)
     }
 
-    def "primitive number fields get automatic default value from their initial assigned value"() {
-        given:
-        def property = mixing.getDescriptor(SQLDefaultValuesEntity.class).findProperty("primitiveIntWithValue")
-                and:
-                SQLDefaultValuesEntity entity = new SQLDefaultValuesEntity()
-                entity.setPrimitiveIntWithValue(12)
-        when: // an empty value is given, its default (50) should be applied
-        property.parseValueFromImport(entity, Value.EMPTY)
-        then:
-        entity.getPrimitiveIntWithValue() == 50
+    @Test
+    fun `primitive number fields get automatic default value from their initial assigned value`() {
+        val property = mixing.getDescriptor(SQLDefaultValuesEntity::class.java).findProperty("primitiveIntWithValue")
+        val sqlDefaultValuesEntity = SQLDefaultValuesEntity()
+        sqlDefaultValuesEntity.primitiveIntWithValue = 12
+        property?.parseValueFromImport(sqlDefaultValuesEntity, Value.EMPTY)
+
+        assertEquals(50, sqlDefaultValuesEntity.primitiveIntWithValue)
     }
 
-    def "amount fields which are initialized with Amount.NOTHING should reset to Amount.NOTHING"() {
-        given:
-        def property = mixing.getDescriptor(SQLDefaultValuesEntity.class).findProperty("amountNothing")
-                and:
-                SQLDefaultValuesEntity entity = new SQLDefaultValuesEntity()
-                entity.setAmountNothing(Amount.of(12))
-        when:
-        property.parseValueFromImport(entity, Value.EMPTY)
-        then:
-        entity.getAmountNothing() == Amount.NOTHING
+    @Test
+    fun `amount fields which are initialized with Amount#NOTHING should reset to Amount#NOTHING`() {
+        val property = mixing.getDescriptor(SQLDefaultValuesEntity::class.java).findProperty("amountNothing")
+        val sqlDefaultValuesEntity = SQLDefaultValuesEntity()
+        val amount = 12
+        sqlDefaultValuesEntity.amountNothing = Amount.of(amount)
+        property?.parseValueFromImport(sqlDefaultValuesEntity, Value.EMPTY)
+
+        assertEquals(Amount.NOTHING, sqlDefaultValuesEntity.amountNothing)
     }
 
-    def "amount fields initialized with a non-empty Amount should persist the default when emptied"() {
-        given:
-        def property = mixing.getDescriptor(SQLDefaultValuesEntity.class).findProperty("amountZero")
+    @ParameterizedTest
+    @MethodSource("provideParametersForAmountInitialized")
+    fun `amount fields initialized with a non-empty Amount should persist the default when emptied`(
+            input: Value,
+            output: Amount
+    ) {
+        val property = mixing.getDescriptor(SQLDefaultValuesEntity::class.java).findProperty("amountZero")
+        val entity = SQLDefaultValuesEntity()
+        val amountSeventySeven = 771
+        entity.amountZero = Amount.of(amountSeventySeven)
+        property?.parseValueFromImport(entity, input)
 
-                expect:
-                SQLDefaultValuesEntity entity = new SQLDefaultValuesEntity()
-                entity.setAmountZero(Amount.of(77))
-                property.parseValueFromImport(entity, input)
-        entity.getAmountZero() == output
+        assertEquals(output, entity.amountZero)
+    }
 
-        where:
-        input                     | output
-        Value.EMPTY               | Amount.ZERO
-        Value.of(5)               | Amount.of(5)
-        Value.of(Amount.NOTHING)  | Amount.ZERO
-        Value.of(Amount.of(44.1)) | Amount.of(44.1)
+    companion object {
+        @Part
+        private lateinit var mixing: Mixing
+
+        @JvmStatic
+        fun provideParametersForValuesInitialized(): Stream<Arguments> {
+            return Stream.of(
+                    Arguments.of("primitiveBoolean", Value.of(false)),
+                    Arguments.of("primitiveBooleanTrue", Value.of(true)),
+                    Arguments.of("booleanObject", Value.EMPTY),
+                    Arguments.of("primitiveInt", Value.of(0)),
+                    Arguments.of("primitiveIntWithValue", Value.of(50)),
+                    Arguments.of("amountWithValue", Value.of(Amount.ONE_HUNDRED)),
+                    Arguments.of("amountZero", Value.of(Amount.ZERO)),
+                    Arguments.of(
+                            "amountNothing",
+                            Value.EMPTY
+                    ), //Amount.NOTHING should be considered null and therefore have no default
+                    Arguments.of("string", Value.EMPTY),
+                    Arguments.of("emptyString", Value.of("")),
+                    Arguments.of("enumWithValue", Value.of(DataTypesEntity.TestEnum.Test2)),
+            )
+        }
+
+        @JvmStatic
+        fun provideParametersForValuesTransformed(): Stream<Arguments> {
+            return Stream.of(
+                    Arguments.of("primitiveBoolean", "0"),
+                    Arguments.of("primitiveBooleanTrue", "1"),
+                    Arguments.of("booleanObject", null),
+                    Arguments.of("primitiveInt", "0"),
+                    Arguments.of("primitiveIntWithValue", "50"),
+                    Arguments.of("amountWithValue", "100.000"),
+                    Arguments.of("amountZero", "0.000"),
+                    Arguments.of("amountNothing", null),
+                    Arguments.of("string", null),
+                    Arguments.of("emptyString", ""),
+                    Arguments.of("filledString", "STRING"),
+                    Arguments.of("enumTest", null),
+                    Arguments.of("enumWithValue", "Test2"),
+            )
+        }
+
+        @JvmStatic
+        fun provideParametersForAmountInitialized(): Stream<Arguments> {
+            val valueFive = 5
+            return Stream.of(
+                    Arguments.of(Value.EMPTY, Amount.ZERO),
+                    Arguments.of(Value.of(5), Amount.of(valueFive)),
+                    Arguments.of(Value.of(Amount.NOTHING), Amount.ZERO),
+                    Arguments.of(Value.of(Amount.of(44.1)), Amount.of(44.1)),
+            )
+        }
     }
 }
