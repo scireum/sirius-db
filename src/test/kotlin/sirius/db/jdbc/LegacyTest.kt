4729c83ee38bb0c14b24510af4121f818188eb77
Mauricio Muler Bodemann <mbo@scireum.de>
2024-02-01 10:27:07 +0100

    Adapts to kotlin syntax
    Fixes: OX-10357

--- src/test/kotlin/sirius/db/jdbc/LegacyTest.kt
+++ src/test/kotlin/sirius/db/jdbc/LegacyTest.kt
@@ -8,68 +8,66 @@
 
 package sirius.db.jdbc
 
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
 import sirius.db.mixing.Mapping
 import sirius.db.mixing.Mixing
-import sirius.kernel.BaseSpecification
+import sirius.kernel.SiriusExtension
 import sirius.kernel.di.std.Part
-
 import java.time.Duration
+import kotlin.test.assertEquals
+import kotlin.test.assertFalse
+import kotlin.test.assertNotNull
 
-class LegacySpec extends BaseSpecification {
+@ExtendWith(SiriusExtension::class)
+class LegacyTest {
+    @Test
+    fun `check if aliasing for columns work`() {
+        val legacyEntity = LegacyEntity()
+        legacyEntity.firstname = "Test"
+        legacyEntity.lastname = "Entity"
+        legacyEntity.composite.street = "Street"
+        legacyEntity.composite.city = "Test-City"
+        legacyEntity.composite.zip = "1245"
+        oma.update(legacyEntity)
 
-    @Part
-    private static OMA oma
-
-            def setupSpec() {
-        oma.getReadyFuture().await(Duration.ofSeconds(60))
-    }
-
-    def "check if aliasing for columns work"() {
-        given:
-        LegacyEntity e = new LegacyEntity()
-        when:
-        e.setFirstname("Test")
-        e.setLastname("Entity")
-        e.getComposite().setStreet("Street")
-        e.getComposite().setCity("Test-City")
-        e.getComposite().setZip("1245")
-        oma.update(e)
-        LegacyEntity fromDB = oma.select(LegacyEntity.class)
+        val fromDB = oma.select(LegacyEntity::class.java)
                 .eq(Mapping.named("firstname"), "Test")
                 .orderAsc(Mapping.named("composite").inner(Mapping.named("street")))
                 .queryFirst()
-                then:
-        !e.isNew()
-        and:
-        fromDB != null
-        and:
-        fromDB.getFirstname() == "Test"
-        and:
-        fromDB.getComposite().getStreet() == "Street"
-        and:
-        oma.getDatabase(Mixing.DEFAULT_REALM).
-        createQuery("SELECT * FROM banana WHERE name1 = 'Test' and street = 'Street'").
-        first().
-        isPresent()
+
+        assertFalse { legacyEntity.isNew }
+        assertNotNull(fromDB)
+        assertEquals("Test", fromDB.firstname)
+        assertEquals("Street", fromDB.composite.street)
+        assertNotNull(
+                oma.getDatabase(Mixing.DEFAULT_REALM)
+                        ?.createQuery("SELECT * FROM banana WHERE name1 = 'Test' and street = 'Street'")?.first()
+        )
     }
 
-    def "updating and change tracking on an entity with aliased columns works"() {
-        given:
-        LegacyEntity e = new LegacyEntity()
-        when:
-        e.setFirstname("Test2")
-        e.setLastname("Entity2")
-        e.getComposite().setStreet("Streeet2")
-        e.getComposite().setCity("Test-City2")
-        e.getComposite().setZip("12452")
-        oma.update(e)
-        and:
-        e.getComposite().setStreet("Street3")
-        oma.update(e)
-        and:
-        e = oma.refreshOrFail(e)
-        then:
-        e.getComposite().getStreet() == "Street3"
+    @Test
+    fun `updating and change tracking on an entity with aliased columns works`() {
+        var legacyEntity = LegacyEntity()
+        legacyEntity.firstname = "Test2"
+        legacyEntity.lastname = "Entity2"
+        legacyEntity.composite.street = "Streeet2"
+        legacyEntity.composite.city = "Test-City2"
+        legacyEntity.composite.zip = "12452"
+        oma.update(legacyEntity)
+        legacyEntity.composite.street = "Street3"
+        oma.update(legacyEntity)
+        legacyEntity = oma.refreshOrFail(legacyEntity)
+
+        assertEquals("Street3", legacyEntity.composite.street)
     }
 
+    companion object {
+        @Part
+        private lateinit var oma: OMA
+
+        fun setupSpec() {
+            oma.readyFuture.await(Duration.ofSeconds(60))
+        }
+    }
 }
