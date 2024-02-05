42bedec312b938fcb42ed36cb20ff4073b52fdb3
Mauricio Muler Bodemann <mbo@scireum.de>
2024-02-05 16:24:50 +0100

    Adapts to kotlin syntax
    Fixes: OX-10357

--- src/test/kotlin/sirius/db/jdbc/clickhouse/ClickhouseTest.kt
+++ src/test/kotlin/sirius/db/jdbc/clickhouse/ClickhouseTest.kt
@@ -8,157 +8,157 @@
 
 package sirius.db.jdbc.clickhouse
 
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
 import sirius.db.jdbc.OMA
 import sirius.db.jdbc.batch.BatchContext
-import sirius.db.jdbc.batch.InsertQuery
-import sirius.kernel.BaseSpecification
+import sirius.kernel.SiriusExtension
 import sirius.kernel.di.std.Part
-
 import java.time.Duration
 import java.time.Instant
 import java.time.LocalDate
 import java.time.temporal.ChronoField
+import kotlin.test.assertEquals
+import kotlin.test.assertFalse
+import kotlin.test.assertNull
+import kotlin.test.assertTrue
 
-class ClickhouseSpec extends BaseSpecification {
+@ExtendWith(SiriusExtension::class)
+class ClickhouseTest {
+    @Test
+    fun `write a test entity and read it back`() {
+        val clickhouseTestEntity = ClickhouseTestEntity()
+        val now = Instant.now().with(ChronoField.MILLI_OF_SECOND, 0)
+        clickhouseTestEntity.dateTime = now
+        clickhouseTestEntity.date = LocalDate.now()
+        clickhouseTestEntity.int8 = 100
+        clickhouseTestEntity.int16 = 30_000
+        clickhouseTestEntity.int32 = 1_000_000_000
+        clickhouseTestEntity.int64 = 10_000_000_000
+        clickhouseTestEntity.string = "This is a long string"
+        clickhouseTestEntity.fixedString = "X"
+        clickhouseTestEntity.setaBooleanSetToTrue(true)
+        clickhouseTestEntity.setaBooleanSetToFalse(false)
+        clickhouseTestEntity.stringList.add("string1").add("string2")
+        clickhouseTestEntity.enumValue = ClickhouseTestEntity.TestEnum.Test2
+        oma.update(clickhouseTestEntity)
+        val readBack =
+                oma.select(ClickhouseTestEntity::class.java).eq(ClickhouseTestEntity.FIXED_STRING, "X").queryFirst()
 
-    @Part
-    static OMA oma
-
-    def setupSpec() {
-        oma.getReadyFuture().await(Duration.ofSeconds(60))
+        assertEquals(100, readBack.int8)
+        assertTrue { readBack.isaBooleanSetToTrue() }
+        assertFalse { readBack.isaBooleanSetToFalse() }
+        assertEquals(30_000, readBack.int16)
+        assertEquals(1_000_000_000, readBack.int32)
+        assertEquals(10_000_000_000, readBack.int64)
+        assertEquals("This is a long string", readBack.string)
+        assertEquals("X", readBack.fixedString)
+        assertEquals(LocalDate.now(), readBack.date)
+        assertEquals(now, readBack.dateTime)
+        assertEquals(listOf("string1", "string2"), readBack.stringList.data())
+        assertEquals(ClickhouseTestEntity.TestEnum.Test2, readBack.enumValue)
     }
 
-    def "write a test entity and read it back"() {
-        given:
-        ClickhouseTestEntity e = new ClickhouseTestEntity()
-        Instant now = Instant.now().with(ChronoField.MILLI_OF_SECOND, 0)
-        e.setDateTime(now)
-        e.setDate(LocalDate.now())
-        e.setInt8(100)
-        e.setInt16(30_000)
-        e.setInt32(1_000_000_000)
-        e.setInt64(10_000_000_000)
-        e.setString("This is a long string")
-        e.setFixedString("X")
-        e.setaBooleanSetToTrue(true)
-        e.setaBooleanSetToFalse(false)
-        e.getStringList().add("string1").add("string2")
-        e.setEnumValue(ClickhouseTestEntity.TestEnum.Test2)
-        when:
-        oma.update(e)
-        then:
-        ClickhouseTestEntity readBack = oma.select(ClickhouseTestEntity.class).
-        eq(ClickhouseTestEntity.FIXED_STRING, "X").
-        queryFirst()
-                and:
-                readBack.getInt8() == 100
-                readBack.isaBooleanSetToTrue() == true
-                readBack.isaBooleanSetToFalse() == false
-                readBack.getInt16() == 30_000
-                readBack.getInt32() == 1_000_000_000
-                readBack.getInt64() == 10_000_000_000
-                readBack.getString() == "This is a long string"
-                readBack.getFixedString() == "X"
-                readBack.getDate() == LocalDate.now()
-                readBack.getDateTime() == now
-                readBack.getStringList().data() == ["string1", "string2"]
-                readBack.getEnumValue() == ClickhouseTestEntity.TestEnum.Test2
-    }
-
-    def "batch insert into clickhouse works"() {
-        setup:
-        BatchContext ctx = new BatchContext({ -> "Test" }, Duration.ofMinutes(2))
-        when:
-        InsertQuery<ClickhouseTestEntity> insert = ctx.insertQuery(
-                ClickhouseTestEntity.class, false)
-        and:
-        for (int i = 0; i < 100; i++) {
-        ClickhouseTestEntity e = new ClickhouseTestEntity()
-        e.setDateTime(Instant.now())
-        e.setDate(LocalDate.now())
-        e.setInt8(i)
-        e.setInt16(i)
-        e.setInt32(i)
-        e.setInt64(i)
-        e.setString("Test")
-        e.setFixedString("B")
-        e.setInt8WithDefault(0)
-        e.setEnumValue(ClickhouseTestEntity.TestEnum.Test1)
-        insert.insert(e, true, true)
-    }
-        and:
+    @Test
+    fun `batch insert into clickhouse works`() {
+        val batchContext = BatchContext({ -> "Test" }, Duration.ofMinutes(2))
+        val insert = batchContext.insertQuery(
+                ClickhouseTestEntity::class.java, false
+        )
+        for (i in 0..99) {
+            val clickhouseTestEntity = ClickhouseTestEntity()
+            clickhouseTestEntity.dateTime = Instant.now()
+            clickhouseTestEntity.date = LocalDate.now()
+            clickhouseTestEntity.int8 = i
+            clickhouseTestEntity.int16 = i
+            clickhouseTestEntity.int32 = i
+            clickhouseTestEntity.int64 = i.toLong()
+            clickhouseTestEntity.string = "Test"
+            clickhouseTestEntity.fixedString = "B"
+            clickhouseTestEntity.setInt8WithDefault(0)
+            clickhouseTestEntity.enumValue = ClickhouseTestEntity.TestEnum.Test1
+            insert.insert(clickhouseTestEntity, true, true)
+        }
         insert.commit()
-        then:
-        oma.select(ClickhouseTestEntity.class).eq(ClickhouseTestEntity.FIXED_STRING, "B").count() == 100
-                cleanup:
-                OMA.LOG.INFO(ctx)
-                ctx.close()
+
+        assertEquals(
+                100,
+                oma.select(ClickhouseTestEntity::class.java).eq(ClickhouseTestEntity.FIXED_STRING, "B").count()
+        )
+
+        OMA.LOG.INFO(batchContext)
+        batchContext.close()
     }
 
-    def "property with default-value is set to default when null in object"() {
-        given:
-        ClickhouseTestEntity e = new ClickhouseTestEntity()
-        e.setDateTime(Instant.now())
-        e.setDate(LocalDate.now())
-        e.setInt8(100)
-        e.setInt16(30_000)
-        e.setInt32(1_000_000_000)
-        e.setInt64(10_000_000_000)
-        e.setString("This is a long string")
-        e.setFixedString("Y")
-        e.setInt8WithDefault(null)
-        when:
-        oma.update(e)
-        then:
-        ClickhouseTestEntity readBack = oma.select(ClickhouseTestEntity.class).
-        eq(ClickhouseTestEntity.FIXED_STRING, "Y").
-        queryFirst()
-                and:
-                readBack.getInt8WithDefault() == 42
+    @Test
+    fun `property with default-value is set to default when null in object`() {
+        val clickhouseTestEntity = ClickhouseTestEntity()
+        clickhouseTestEntity.dateTime = Instant.now()
+        clickhouseTestEntity.date = LocalDate.now()
+        clickhouseTestEntity.int8 = 100
+        clickhouseTestEntity.int16 = 30_000
+        clickhouseTestEntity.int32 = 1_000_000_000
+        clickhouseTestEntity.int64 = 10_000_000_000
+        clickhouseTestEntity.string = "This is a long string"
+        clickhouseTestEntity.fixedString = "Y"
+        clickhouseTestEntity.setInt8WithDefault(null)
+        oma.update(clickhouseTestEntity)
+        val readBack =
+                oma.select(ClickhouseTestEntity::class.java).eq(ClickhouseTestEntity.FIXED_STRING, "Y").queryFirst()
+
+        assertEquals(42, readBack.int8WithDefault)
     }
 
-    def "property with default-value is set to actual value when set"() {
-        given:
-        ClickhouseTestEntity e = new ClickhouseTestEntity()
-        e.setDateTime(Instant.now())
-        e.setDate(LocalDate.now())
-        e.setInt8(100)
-        e.setInt16(30_000)
-        e.setInt32(1_000_000_000)
-        e.setInt64(10_000_000_000)
-        e.setString("This is a long string")
-        e.setFixedString("Z")
-        e.setInt8WithDefault(17)
-        when:
-        oma.update(e)
-        then:
-        ClickhouseTestEntity readBack = oma.select(ClickhouseTestEntity.class).
-        eq(ClickhouseTestEntity.FIXED_STRING, "Z").
-        queryFirst()
-                and:
-                readBack.getInt8WithDefault() == 17
+    @Test
+    fun `property with default-value is set to actual value when set`() {
+        val clickhouseTestEntity = ClickhouseTestEntity()
+        clickhouseTestEntity.dateTime = Instant.now()
+        clickhouseTestEntity.date = LocalDate.now()
+        clickhouseTestEntity.int8 = 100
+        clickhouseTestEntity.int16 = 30_000
+        clickhouseTestEntity.int32 = 1_000_000_000
+        clickhouseTestEntity.int64 = 10_000_000_000
+        clickhouseTestEntity.string = "This is a long string"
+        clickhouseTestEntity.fixedString = "Z"
+        clickhouseTestEntity.setInt8WithDefault(17)
+        oma.update(clickhouseTestEntity)
+        val readBack = oma.select(ClickhouseTestEntity::class.java).eq(
+                ClickhouseTestEntity.FIXED_STRING,
+                "Z"
+        ).queryFirst()
+
+        assertEquals(17, readBack.int8WithDefault)
     }
 
-    def "nullable property can be null"() {
-        given:
-        ClickhouseTestEntity e = new ClickhouseTestEntity()
-        e.setDateTime(Instant.now())
-        e.setDate(LocalDate.now())
-        e.setInt8(100)
-        e.setInt16(30_000)
-        e.setInt32(1_000_000_000)
-        e.setInt64(10_000_000_000)
-        e.setString("This is a long string")
-        e.setFixedString("A")
-        e.setInt8WithDefault(17)
-        e.setNullable(null)
-        when:
-        oma.update(e)
-        then:
-        ClickhouseTestEntity readBack = oma.select(ClickhouseTestEntity.class).
-        eq(ClickhouseTestEntity.FIXED_STRING, "A").
-        queryFirst()
-                and:
-                readBack.getNullable() == null
+    @Test
+    fun `nullable property can be null`() {
+        val clickhouseTestEntity = ClickhouseTestEntity()
+        clickhouseTestEntity.dateTime = Instant.now()
+        clickhouseTestEntity.date = LocalDate.now()
+        clickhouseTestEntity.int8 = 100
+        clickhouseTestEntity.int16 = 30_000
+        clickhouseTestEntity.int32 = 1_000_000_000
+        clickhouseTestEntity.int64 = 10_000_000_000
+        clickhouseTestEntity.string = "This is a long string"
+        clickhouseTestEntity.fixedString = "A"
+        clickhouseTestEntity.setInt8WithDefault(17)
+        clickhouseTestEntity.nullable = null
+        oma.update(clickhouseTestEntity)
+        val readBack =
+                oma.select(ClickhouseTestEntity::class.java).eq(ClickhouseTestEntity.FIXED_STRING, "A").queryFirst()
+
+        assertNull(readBack.nullable)
+    }
+
+    companion object {
+        @Part
+        private lateinit var oma: OMA
+
+        @BeforeAll
+        @JvmStatic
+        fun setupSpec() {
+            oma.readyFuture.await(Duration.ofSeconds(60))
+        }
     }
 }
