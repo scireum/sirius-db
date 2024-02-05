1917201754ae8a439a20c7f6aae3227b86547325
Mauricio Muler Bodemann <mbo@scireum.de>
2024-02-05 14:15:41 +0100

    Adapts to kotlin syntax
    Fixes: OX-10357

--- src/test/kotlin/sirius/db/jdbc/TransformedQueryTest.kt
+++ src/test/kotlin/sirius/db/jdbc/TransformedQueryTest.kt
@@ -8,69 +8,70 @@
 
 package sirius.db.jdbc
 
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
 import sirius.db.mixing.Mixing
-import sirius.kernel.BaseSpecification
+import sirius.kernel.SiriusExtension
 import sirius.kernel.di.std.Part
-
 import java.time.Duration
-import java.util.function.Function
 import java.util.stream.Collectors
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
-class TransformedQuerySpec extends BaseSpecification {
+@ExtendWith(SiriusExtension::class)
+class TransformedQueryTest {
+    @Test
+    fun `transform works when reading a test entity`() {
+        val sqlQuery = oma.getDatabase(Mixing.DEFAULT_REALM)
+                ?.createQuery("SELECT * FROM transformedquerytestentity ORDER BY value ASC")
+        val testEntity = oma.transform(TransformedQueryTestEntity::class.java, sqlQuery).queryFirst()
+        val testEntities = oma.transform(TransformedQueryTestEntity::class.java, sqlQuery).queryList()
 
-    @Part
-    static OMA oma
-
-    def setupSpec() {
-        oma.getReadyFuture().await(Duration.ofSeconds(60))
-
-        TransformedQueryTestEntity e = new TransformedQueryTestEntity()
-        e.setValue("Test")
-        oma.update(e)
-        e = new TransformedQueryTestEntity()
-        e.setValue("Hello")
-        oma.update(e)
-        e = new TransformedQueryTestEntity()
-        e.setValue("World")
-        oma.update(e)
+        assertEquals("Hello", testEntity.value)
+        assertEquals(
+                listOf("Hello", "Test", "World"),
+                testEntities.stream().map { x -> x.value }.collect(Collectors.toList())
+        )
     }
 
-    def "transform works when reading a test entity"() {
-        given:
-        SQLQuery qry = oma.getDatabase(Mixing.DEFAULT_REALM).
-        createQuery("SELECT * FROM transformedquerytestentity ORDER BY value ASC")
-        when:
-        def e = oma.transform(TransformedQueryTestEntity.class, qry).queryFirst()
-        and:
-        def es = oma.transform(TransformedQueryTestEntity.class, qry).queryList()
-        then:
-        e.getValue() == "Hello"
-        and:
-        es.stream().map({ x -> x.getValue() } as Function).collect(Collectors.toList()) == ["Hello", "Test", "World"]
+    @Test
+    fun `transform works when reading a test entity with alias`() {
+        val sqlQuery = oma.getDatabase(Mixing.DEFAULT_REALM)
+                ?.createQuery("SELECT id as x_id, value as x_value  FROM transformedquerytestentity ORDER BY value ASC")
+        val testEntity = oma.transform(TransformedQueryTestEntity::class.java, "x", sqlQuery).first()
+
+        assertTrue { testEntity.isPresent }
+        assertEquals("Hello", testEntity.get().value)
     }
 
-    def "transform works when reading a test entity with alias"() {
-        given:
-        SQLQuery qry = oma.getDatabase(Mixing.DEFAULT_REALM).
-        createQuery("SELECT id as x_id, value as x_value  FROM transformedquerytestentity ORDER BY value ASC")
-        when:
-        def e = oma.transform(TransformedQueryTestEntity.class, "x", qry).first()
-        then:
-        e.isPresent()
-        and:
-        e.get().getValue() == "Hello"
+    @Test
+    fun `transform works when reading a test entity with a computed column`() {
+        val sqlQuery = oma.getDatabase(Mixing.DEFAULT_REALM)
+                ?.createQuery("SELECT id, value, 'x' as test FROM transformedquerytestentity ORDER BY value ASC")
+        val testEntity = oma.transform(TransformedQueryTestEntity::class.java, sqlQuery).queryFirst()
+
+        assertEquals("Hello", testEntity.value)
+        assertEquals("x", testEntity.getFetchRow().getValue("test").asString())
     }
 
-    def "transform works when reading a test entity with a computed column"() {
-        given:
-        SQLQuery qry = oma.getDatabase(Mixing.DEFAULT_REALM).
-        createQuery("SELECT id, value, 'x' as test FROM transformedquerytestentity ORDER BY value ASC")
-        when:
-        def e = oma.transform(TransformedQueryTestEntity.class, qry).queryFirst()
-        then:
-        e.getValue() == "Hello"
-        and:
-        e.getFetchRow().getValue("test").asString() == "x"
-    }
+    companion object {
+        @Part
+        private lateinit var oma: OMA
 
+        @BeforeAll
+        @JvmStatic
+        fun setupSpec() {
+            oma.readyFuture.await(Duration.ofSeconds(60))
+            var transformedQueryTestEntity = TransformedQueryTestEntity()
+            transformedQueryTestEntity.value = "Test"
+            oma.update(transformedQueryTestEntity)
+            transformedQueryTestEntity = TransformedQueryTestEntity()
+            transformedQueryTestEntity.value = "Hello"
+            oma.update(transformedQueryTestEntity)
+            transformedQueryTestEntity = TransformedQueryTestEntity()
+            transformedQueryTestEntity.value = "World"
+            oma.update(transformedQueryTestEntity)
+        }
+    }
 }
