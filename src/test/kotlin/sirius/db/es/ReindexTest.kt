50268311fe16b057d0bfbb32ad222490f293824d
Mauricio Muler Bodemann <mbo@scireum.de>
2024-02-06 16:05:17 +0100

    Adapts to kotlin syntax
    Fixes: OX-10357

--- src/test/kotlin/sirius/db/es/ReindexTest.kt
+++ src/test/kotlin/sirius/db/es/ReindexTest.kt
@@ -8,40 +8,41 @@
 
 package sirius.db.es
 
-import sirius.kernel.BaseSpecification
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import sirius.kernel.SiriusExtension
 import sirius.kernel.commons.Wait
 import sirius.kernel.di.std.Part
+import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
+import kotlin.test.assertTrue
 
-class ReindexSpec extends BaseSpecification {
+@ExtendWith(SiriusExtension::class)
+class ReindexTest {
+    @Test
+    fun `reindex and move alias works`() {
+        val elasticTestEntity = ElasticTestEntity()
+        elasticTestEntity.age = 10
+        elasticTestEntity.firstname = "test"
+        elasticTestEntity.lastname = "test"
+        elastic.update(elasticTestEntity)
+        elastic.refresh(ElasticTestEntity::class.java)
+        elastic.getLowLevelClient()
+                .startReindex(elastic.determineReadAlias(elasticTestEntity.descriptor), "reindex-test")
+        Wait.seconds((2).toDouble())
 
-    @Part
-    private static Elastic elastic
+        assertTrue { elastic.getLowLevelClient().indexExists("reindex-test") }
 
-            def "reindex and move alias works"() {
-        given:
-        ElasticTestEntity e = new ElasticTestEntity()
-        e.setAge(10)
-        e.setFirstname("test")
-        e.setLastname("test")
-        and:
-        elastic.update(e)
-        and:
-        elastic.refresh(ElasticTestEntity.class)
+        elastic.getLowLevelClient()
+                .createOrMoveAlias(elastic.determineReadAlias(elasticTestEntity.descriptor), "reindex-test")
+        val indicesForAlias = elastic.determineEffectiveIndex(elasticTestEntity.descriptor)
 
-                when:
-        elastic.getLowLevelClient().startReindex(elastic.determineReadAlias(e.getDescriptor()), "reindex-test")
-        and:
-        Wait.seconds(2)
-        then:
-        elastic.getLowLevelClient().indexExists("reindex-test")
+        assertEquals("reindex-test", indicesForAlias)
+        assertNotNull(elastic.find(ElasticTestEntity::class.java, elasticTestEntity.id))
+    }
 
-        when:
-        elastic.getLowLevelClient().createOrMoveAlias(elastic.determineReadAlias(e.getDescriptor()), "reindex-test")
-        and:
-        def indicesForAlias = elastic.determineEffectiveIndex(e.getDescriptor())
-        then:
-        indicesForAlias == "reindex-test"
-        and:
-        elastic.find(ElasticTestEntity.class, e.getId()).isPresent()
+    companion object {
+        @Part
+        private lateinit var elastic: Elastic
     }
 }
