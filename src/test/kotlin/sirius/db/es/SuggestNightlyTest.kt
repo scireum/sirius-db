2c0ff579a0e9fc7361bd6eedd5eed2205fc1a1e5
Mauricio Muler Bodemann <mbo@scireum.de>
2024-02-06 16:35:37 +0100

    Adapts to kotlin syntax
    Fixes: OX-10357

--- src/test/kotlin/sirius/db/es/SuggestNightlyTest.kt
+++ src/test/kotlin/sirius/db/es/SuggestNightlyTest.kt
@@ -9,144 +9,148 @@
 package sirius.db.es
 
 
+import com.fasterxml.jackson.databind.node.ObjectNode
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Tag
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import sirius.db.es.constraints.BoolQueryBuilder
-
 import sirius.db.es.suggest.SuggesterBuilder
 import sirius.kernel.SiriusExtension
 import sirius.kernel.Tags
 import sirius.kernel.commons.Json
 import sirius.kernel.di.std.Part
-
 import java.time.Duration
-import java.util.function.Predicate
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
 @ExtendWith(SiriusExtension::class)
 @Tag(Tags.NIGHTLY)
 class SuggestNightlyTest {
-
-    @Part
-    private static Elastic elastic
-
-            def setupSpec() {
-        elastic.getReadyFuture().await(Duration.ofSeconds(60))
-    }
-
-    def "term suggest works"() {
-        when:
+    @Test
+    fun `term suggest works`() {
         // Add enough data to make sure elastic returns all expected suggestions
-        for (int i = 0; i < 100; i++) {
-        def entity1 = new SuggestTestEntity()
-        entity1.setContent("HSS drill bit")
-        elastic.update(entity1)
+        for (i in 0..99) {
+            val entity1 = SuggestTestEntity()
+            entity1.content = "HSS drill bit"
+            elastic.update(entity1)
+            val entity2 = SuggestTestEntity()
+            entity2.content = "Salmon with dill"
+            elastic.update(entity2)
+        }
+        elastic.refresh(SuggestTestEntity::class.java)
+        val suggestParts = elastic.suggest(SuggestTestEntity::class.java)
+                .withTermSuggester("test", SuggestTestEntity.CONTENT, "HSS dril bitt").execute().getSuggestions("test")
 
-        def entity2 = new SuggestTestEntity()
-        entity2.setContent("Salmon with dill")
-        elastic.update(entity2)
-    }
-        and:
-        elastic.refresh(SuggestTestEntity.class)
-                def suggestParts = elastic.suggest(SuggestTestEntity.class)
-                .withTermSuggester("test", SuggestTestEntity.CONTENT, "HSS dril bitt")
-                .execute()
-                .getSuggestions("test")
-                then:
-                suggestParts.size() == 3
-                suggestParts.get(0).getTermSuggestions().size() == 0
-                suggestParts.get(1).getTermSuggestions().size() == 2
-                suggestParts.get(2).getTermSuggestions().size() == 1
-                and:
-                suggestParts
-                .get(1)
-                .getTermSuggestions()
-                .stream()
-                .anyMatch({ option -> option.getText().equalsIgnoreCase("drill") } as Predicate)
-                suggestParts
-                .get(1)
-                .getTermSuggestions()
-                .stream()
-                .anyMatch({ option -> option.getText().equalsIgnoreCase("dill") } as Predicate)
-                suggestParts.get(2).getTermSuggestions().get(0).getText().equalsIgnoreCase("bit")
+        assertEquals(3, suggestParts.size)
+        assertEquals(0, suggestParts[0].termSuggestions.size)
+        assertEquals(2, suggestParts[1].termSuggestions.size)
+        assertEquals(1, suggestParts[2].termSuggestions.size)
+
+        assertTrue {
+            suggestParts[1].termSuggestions.stream()
+                    .anyMatch { option -> option.text.equals("drill", ignoreCase = true) }
+        }
+        assertTrue {
+            suggestParts[1].termSuggestions.stream()
+                    .anyMatch { option -> option.text.equals("dill", ignoreCase = true) }
+        }
+        assertTrue { suggestParts[2].termSuggestions[0].text.equals("bit", ignoreCase = true) }
     }
 
-    def "phrase suggest works"() {
-        when:
+    @Test
+    fun `phrase suggest works`() {
         // Add enough data to make sure elastic returns all expected suggestions
-        for (int i = 0; i < 100; i++) {
-        def entity1 = new SuggestTestEntity()
-        entity1.setContent("HSS drill bit")
-        elastic.update(entity1)
+        for (i in 0..99) {
+            val entity1 = SuggestTestEntity()
+            entity1.content = "HSS drill bit"
+            elastic.update(entity1)
+            val entity2 = SuggestTestEntity()
+            entity2.content = "Salmon with dill"
+            elastic.update(entity2)
+        }
+        elastic.refresh(SuggestTestEntity::class.java)
+        val suggestOptions = elastic.suggest(SuggestTestEntity::class.java).withSuggester(
+                SuggesterBuilder(SuggesterBuilder.TYPE_PHRASE, "test").on(SuggestTestEntity.CONTENT)
+                        .forText("dril potatoes")
+        ).execute().getSingleTermSuggestions("test")
 
-        def entity2 = new SuggestTestEntity()
-        entity2.setContent("Salmon with dill")
-        elastic.update(entity2)
-    }
-        and:
-        elastic.refresh(SuggestTestEntity.class)
-                def suggestOptions = elastic.suggest(SuggestTestEntity.class)
-                .withSuggester(new SuggesterBuilder(SuggesterBuilder.TYPE_PHRASE, "test")
-                .on(SuggestTestEntity.CONTENT).forText("dril potatoes"))
-                .execute()
-                .getSingleTermSuggestions("test")
-                then:
-                suggestOptions.size() == 2
-                and:
-                suggestOptions.stream().anyMatch({ option -> option.getText().equalsIgnoreCase("drill potatoes") } as Predicate)
-                suggestOptions.stream().anyMatch({ option -> option.getText().equalsIgnoreCase("dill potatoes") } as Predicate)
+        assertEquals(2, suggestOptions.size)
+        assertTrue {
+            suggestOptions.stream().anyMatch { option -> option.text.equals("drill potatoes", ignoreCase = true) }
+        }
+        assertTrue {
+            suggestOptions.stream().anyMatch { option -> option.text.equals("dill potatoes", ignoreCase = true) }
+        }
     }
 
-    def "phrase suggest with a collate query works"() {
-        when:
+    @Test
+    fun `phrase suggest with a collate query works`() {
         // Add enough data as the collate query only runs on one shard
-        for (int i = 0; i < 100; i++) {
-        def entity1 = new SuggestTestEntity()
-        entity1.setContent("Salsa Tomaten")
-        entity1.setShop(1)
-        elastic.update(entity1)
+        for (i in 0..99) {
+            val suggestTestEntity1 = SuggestTestEntity()
+            suggestTestEntity1.content = "Salsa Tomaten"
+            suggestTestEntity1.shop = 1
+            elastic.update(suggestTestEntity1)
+            val suggestTestEntity2 = SuggestTestEntity()
+            suggestTestEntity2.content = "Kartoffeln mit Salz"
+            suggestTestEntity2.shop = 1
+            elastic.update(suggestTestEntity2)
+            val suggestTestEntity3 = SuggestTestEntity()
+            suggestTestEntity3.content = "Kartoffel Sale"
+            suggestTestEntity3.shop = 2
+            elastic.update(suggestTestEntity3)
+        }
+        val matchPhrase = Json.createObject().set<ObjectNode>(
+                "match",
+                Json.createObject().set(
+                        SuggestTestEntity.CONTENT.name,
+                        Json.createObject().put("query", "{{suggestion}}").put("operator", "and")
+                )
+        )
+        val matchShop = Elastic.FILTERS.eq(SuggestTestEntity.SHOP, 1)
+        elastic.refresh(SuggestTestEntity::class.java)
+        val suggestOptions = elastic.suggest(SuggestTestEntity::class.java).withSuggester(
+                SuggesterBuilder(SuggesterBuilder.TYPE_PHRASE, "test").on(SuggestTestEntity.CONTENT)
+                        .forText("Sals Kartoffeln")
+                        .collate(BoolQueryBuilder().must(matchPhrase).must(matchShop).build(), true)
+        ).execute().getSingleTermSuggestions("test")
 
-        def entity2 = new SuggestTestEntity()
-        entity2.setContent("Kartoffeln mit Salz")
-        entity2.setShop(1)
-        elastic.update(entity2)
-
-        def entity3 = new SuggestTestEntity()
-        entity3.setContent("Kartoffel Sale")
-        entity3.setShop(2)
-        elastic.update(entity3)
+        assertEquals(3, suggestOptions.size)
+        assertTrue {
+            suggestOptions.stream().anyMatch { option ->
+                option.text.equals(
+                        "Salsa Kartoffeln",
+                        ignoreCase = true
+                ) && !option.isCollateMatch
+            }
+        }
+        assertTrue {
+            suggestOptions.stream().anyMatch { option ->
+                option.text.equals(
+                        "Sale Kartoffeln",
+                        ignoreCase = true
+                ) && !option.isCollateMatch
+            }
+        }
+        assertTrue {
+            suggestOptions.stream().anyMatch { option ->
+                option.text.equals(
+                        "Salz Kartoffeln",
+                        ignoreCase = true
+                ) && option.isCollateMatch
+            }
+        }
     }
-        and:
-        def matchPhrase = Json.createObject().set("match",
-                Json.createObject().set(SuggestTestEntity.CONTENT.getName(),
-                        Json.createObject().put("query",
-                                "{{suggestion}}")
-                                .put("operator",
-                                        "and")))
 
-        def matchShop = Elastic.FILTERS.eq(SuggestTestEntity.SHOP, 1)
+    companion object {
+        @Part
+        private lateinit var elastic: Elastic
 
-
-        and:
-        elastic.refresh(SuggestTestEntity.class)
-                def suggestOptions = elastic.suggest(SuggestTestEntity.class)
-                .withSuggester(new SuggesterBuilder(SuggesterBuilder.TYPE_PHRASE, "test")
-                .on(SuggestTestEntity.CONTENT).forText("Sals Kartoffeln")
-                .collate(new BoolQueryBuilder().must(matchPhrase)
-                        .must(matchShop)
-                        .build(),
-                        true))
-                .execute()
-                .getSingleTermSuggestions("test")
-                then:
-                suggestOptions.size() == 3
-                and:
-                suggestOptions.stream()
-                .anyMatch({ option -> option.getText().equalsIgnoreCase("Salsa Kartoffeln") && !option.isCollateMatch() } as Predicate)
-
-                suggestOptions.stream()
-                .anyMatch({ option -> option.getText().equalsIgnoreCase("Sale Kartoffeln") && !option.isCollateMatch() } as Predicate)
-
-                suggestOptions.stream()
-                .anyMatch({ option -> option.getText().equalsIgnoreCase("Salz Kartoffeln") && option.isCollateMatch() } as Predicate)
+        @BeforeAll
+        @JvmStatic
+        fun setupSpec() {
+            elastic.getReadyFuture().await(Duration.ofSeconds(60))
+        }
     }
 }
