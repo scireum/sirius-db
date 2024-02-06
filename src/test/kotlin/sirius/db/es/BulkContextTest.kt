6602fdf9461f9aef6048e7604cdf2f72bba25622
Mauricio Muler Bodemann <mbo@scireum.de>
2024-02-06 10:43:10 +0100

    Adapts to kotlin syntax
    Fixes: OX-10357

--- src/test/kotlin/sirius/db/es/BulkContextTest.kt
+++ src/test/kotlin/sirius/db/es/BulkContextTest.kt
@@ -8,127 +8,132 @@
 
 package sirius.db.es
 
-import sirius.kernel.BaseSpecification
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.api.extension.ExtendWith
+import sirius.kernel.SiriusExtension
 import sirius.kernel.di.std.Part
 import sirius.kernel.health.HandledException
 
 import java.time.Duration
+import kotlin.test.assertEquals
+import kotlin.test.assertFalse
+import kotlin.test.assertNotNull
+import kotlin.test.assertTrue
 
-class BulkContextSpec extends BaseSpecification {
+@ExtendWith(SiriusExtension::class)
+class BulkContextTest {
+    @Test
+    fun `batch insert works`() {
+        val bulkContext = elastic.batch()
+        elastic.select(BatchTestEntity::class.java).delete()
+        bulkContext.tryUpdate(BatchTestEntity().withValue(1))
+        bulkContext.tryUpdate(BatchTestEntity().withValue(2))
+        bulkContext.tryUpdate(BatchTestEntity().withValue(3))
+        bulkContext.commit()
+        elastic.refresh(BatchTestEntity::class.java)
 
-    @Part
-    private static Elastic elastic
-
-            def setupSpec() {
-        elastic.getReadyFuture().await(Duration.ofSeconds(60))
+        assertEquals(3, elastic.select(BatchTestEntity::class.java).count())
     }
 
-    def "batch insert works"() {
-        setup:
-        BulkContext btx = elastic.batch()
-        elastic.select(BatchTestEntity.class).delete()
-                when:
-        btx.tryUpdate(new BatchTestEntity().withValue(1))
-        btx.tryUpdate(new BatchTestEntity().withValue(2))
-        btx.tryUpdate(new BatchTestEntity().withValue(3))
-        btx.commit()
-        elastic.refresh(BatchTestEntity.class)
-                then:
-                elastic.select(BatchTestEntity.class).count() == 3
+    @Test
+    fun `batch insert with routing works`() {
+        val bulkContext = elastic.batch()
+        elastic.select(RoutedBatchTestEntity::class.java).delete()
+        bulkContext.tryUpdate(RoutedBatchTestEntity().withValue(1).withValue1(5))
+        bulkContext.tryUpdate(RoutedBatchTestEntity().withValue(2).withValue1(5))
+        bulkContext.tryUpdate(RoutedBatchTestEntity().withValue(3).withValue1(5))
+        bulkContext.commit()
+        elastic.refresh(RoutedBatchTestEntity::class.java)
+
+        assertEquals(3,
+                elastic.select(RoutedBatchTestEntity::class.java).routing("5").eq(RoutedBatchTestEntity.VALUE1, 5)
+                        .count()
+        )
     }
 
-    def "batch insert with routing works"() {
-        setup:
-        BulkContext btx = elastic.batch()
-        elastic.select(RoutedBatchTestEntity.class).delete()
-                when:
-        btx.tryUpdate(new RoutedBatchTestEntity().withValue(1).withValue1(5))
-        btx.tryUpdate(new RoutedBatchTestEntity().withValue(2).withValue1(5))
-        btx.tryUpdate(new RoutedBatchTestEntity().withValue(3).withValue1(5))
-        btx.commit()
-        elastic.refresh(RoutedBatchTestEntity.class)
-                then:
-                elastic.select(RoutedBatchTestEntity.class).routing("5").eq(RoutedBatchTestEntity.VALUE1, 5).count() == 3
+    @Test
+    fun `optimistic locking with batchcontext works`() {
+        val bulkContext = elastic.batch()
+        elastic.select(BatchTestEntity::class.java).delete()
+        val modified = BatchTestEntity().withValue(100)
+        elastic.update(modified)
+        elastic.refresh(BatchTestEntity::class.java)
+        val original = elastic.refreshOrFail(modified)
+        elastic.update(modified.withValue(200))
+        elastic.refresh(BatchTestEntity::class.java)
+        bulkContext.tryUpdate(original.withValue(150))
+        bulkContext.commit()
+        elastic.refresh(BatchTestEntity::class.java)
+
+        assertEquals(200, elastic.refreshOrFail(original).value)
     }
 
-    def "optimistic locking with batchcontext works"() {
-        setup:
-        BulkContext btx = elastic.batch()
-        elastic.select(BatchTestEntity.class).delete()
-                when:
-        BatchTestEntity modified = new BatchTestEntity().withValue(100)
-                elastic.update(modified)
-                elastic.refresh(BatchTestEntity.class)
-                BatchTestEntity original = elastic.refreshOrFail(modified)
-                elastic.update(modified.withValue(200))
-                elastic.refresh(BatchTestEntity.class)
-                btx.tryUpdate(original.withValue(150))
-                btx.commit()
-                elastic.refresh(BatchTestEntity.class)
-                then:
-                elastic.refreshOrFail(original).getValue() == 200
+    @Test
+    fun `overwriting with batchcontext works`() {
+        val bulkContext = elastic.batch()
+        elastic.select(BatchTestEntity::class.java).delete()
+        val modified = BatchTestEntity().withValue(100)
+        elastic.update(modified)
+        elastic.refresh(BatchTestEntity::class.java)
+        val original = elastic.refreshOrFail(modified)
+        elastic.update(modified.withValue(200))
+        elastic.refresh(BatchTestEntity::class.java)
+        bulkContext.overwrite(original.withValue(150))
+        bulkContext.commit()
+        elastic.refresh(BatchTestEntity::class.java)
+
+        assertEquals(150, elastic.refreshOrFail(original).value)
     }
 
-    def "overwriting with batchcontext works"() {
-        setup:
-        BulkContext btx = elastic.batch()
-        elastic.select(BatchTestEntity.class).delete()
-                when:
-        BatchTestEntity modified = new BatchTestEntity().withValue(100)
-                elastic.update(modified)
-                elastic.refresh(BatchTestEntity.class)
-                BatchTestEntity original = elastic.refreshOrFail(modified)
-                elastic.update(modified.withValue(200))
-                elastic.refresh(BatchTestEntity.class)
-                btx.overwrite(original.withValue(150))
-                btx.commit()
-                elastic.refresh(BatchTestEntity.class)
-                then:
-                elastic.refreshOrFail(original).getValue() == 150
+    @Test
+    fun `delete with batchcontext works`() {
+        val bulkContext = elastic.batch()
+        elastic.select(BatchTestEntity::class.java).delete()
+        val batchTestEntity = BatchTestEntity ().withValue(100)
+        elastic.update(batchTestEntity)
+        elastic.refresh(BatchTestEntity::class.java)
+        bulkContext.tryDelete(batchTestEntity)
+        bulkContext.commit()
+        elastic.refresh(BatchTestEntity::class.java)
+
+        assertNotNull(elastic.find(BatchTestEntity::class.java, batchTestEntity.id))
     }
 
-    def "delete with batchcontext works"() {
-        setup:
-        BulkContext btx = elastic.batch()
-        elastic.select(BatchTestEntity.class).delete()
-                when:
-        BatchTestEntity test = new BatchTestEntity().withValue(100)
-                elastic.update(test)
-                elastic.refresh(BatchTestEntity.class)
-                btx.tryDelete(test)
-                btx.commit()
-                elastic.refresh(BatchTestEntity.class)
-                then:
-                !elastic.find(BatchTestEntity.class, test.getId()).isPresent()
+    @Test
+    fun `beforeSave in bulkContext works`() {
+        val bulkContext = elastic.batch()
+        val elasticTestEntity = ElasticTestEntity ()
+        elasticTestEntity.firstname = null
+
+        assertThrows<HandledException> { bulkContext.tryUpdate(elasticTestEntity) }
     }
 
-    def "beforeSave in bulkContext works"() {
-        setup:
-        BulkContext btx = elastic.batch()
-        ElasticTestEntity test = new ElasticTestEntity()
-        test.setFirstname(null)
-        when:
-        btx.tryUpdate(test)
-        then:
-        thrown(HandledException)
+    @Test
+    fun `getFailedIds() works`() {
+        val bulkContext = elastic.batch()
+        elastic.select(BatchTestEntity::class.java).delete()
+        val batchTestEntity1 = BatchTestEntity ().withValue(1)
+        val batchTestEntity2 = BatchTestEntity ().withValue(10)
+        elastic.update(batchTestEntity1)
+        val refreshed = elastic . refreshOrFail (batchTestEntity1)
+        elastic.update(batchTestEntity1.withValue(2))
+        val result = bulkContext . tryUpdate (refreshed.withValue(3)).tryUpdate(batchTestEntity2).commit()
+
+        assertFalse { result.isSuccessful }
+        assertEquals(1,result.failedIds.size)
+        assertTrue { result.getFailedIds().contains(refreshed.id) }
     }
 
-    def "getFailedIds() works"() {
-        setup:
-        BulkContext btx = elastic.batch()
-        elastic.select(BatchTestEntity.class).delete()
-                BatchTestEntity test = new BatchTestEntity().withValue(1)
-                BatchTestEntity test2 = new BatchTestEntity().withValue(10)
-                when:
-        elastic.update(test)
-        def refreshed = elastic.refreshOrFail(test)
-        elastic.update(test.withValue(2))
-        def result = btx.tryUpdate(refreshed.withValue(3)).tryUpdate(test2).commit()
-        then:
-        !result.isSuccessful()
-        and:
-        result.getFailedIds().size() == 1
-        and:
-        result.getFailedIds().contains(refreshed.getId())
+    companion object {
+        @Part
+        private lateinit var elastic: Elastic
+
+        @BeforeAll
+        @JvmStatic
+        fun setupSpec() {
+            elastic.getReadyFuture().await(Duration.ofSeconds(60))
+        }
     }
 }
