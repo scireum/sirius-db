5dd32139bf184cce420659bde0c6f9df89961f81
Mauricio Muler Bodemann <mbo@scireum.de>
2024-02-07 13:47:33 +0100

    Moves ESFilledTest to new file
    Fixes: OX-10357

---
+++ src/test/kotlin/sirius/db/es/properties/ESFilledTest.kt
@@ -0,0 +1,62 @@
+/*
+ * Made with all the love in the world
+ * by scireum in Remshalden, Germany
+ *
+ * Copyright by scireum GmbH
+ * http://www.scireum.de - info@scireum.de
+ */
+
+package sirius.db.es.properties
+
+import sirius.db.es.Elastic
+import sirius.kernel.BaseSpecification
+import sirius.kernel.di.std.Part
+
+class ESFilledSpec extends BaseSpecification {
+
+    @Part
+    private static Elastic elastic
+
+            def "filled/notFilled/exists query works"() {
+        setup:
+        ESFilledEntity fieldFilled = new ESFilledEntity()
+        fieldFilled.setTestField("test")
+        ESFilledEntity fieldNotFilled = new ESFilledEntity()
+        when:
+        elastic.update(fieldFilled)
+        elastic.update(fieldNotFilled)
+        elastic.refresh(ESFilledEntity.class)
+                then:
+                elastic.select(ESFilledEntity.class)
+                .eq(ESFilledEntity.TEST_FIELD, null)
+                .queryFirst()
+                .getIdAsString() == fieldNotFilled.getIdAsString()
+                elastic.select(ESFilledEntity.class)
+                .eq(ESFilledEntity.TEST_FIELD, null)
+                .count() == 1
+
+                elastic.select(ESFilledEntity.class)
+                .where(Elastic.FILTERS.notFilled(ESFilledEntity.TEST_FIELD))
+                .queryFirst()
+                .getIdAsString() == fieldNotFilled.getIdAsString()
+                elastic.select(ESFilledEntity.class)
+                .where(Elastic.FILTERS.notFilled(ESFilledEntity.TEST_FIELD))
+                .count() == 1
+
+                elastic.select(ESFilledEntity.class)
+                .ne(ESFilledEntity.TEST_FIELD, null)
+                .queryFirst()
+                .getIdAsString() == fieldFilled.getIdAsString()
+                elastic.select(ESFilledEntity.class)
+                .ne(ESFilledEntity.TEST_FIELD, null)
+                .count() == 1
+
+                elastic.select(ESFilledEntity.class)
+                .where(Elastic.FILTERS.filled(ESFilledEntity.TEST_FIELD))
+                .queryFirst()
+                .getIdAsString() == fieldFilled.getIdAsString()
+                elastic.select(ESFilledEntity.class)
+                .where(Elastic.FILTERS.filled(ESFilledEntity.TEST_FIELD))
+                .count() == 1
+    }
+}
