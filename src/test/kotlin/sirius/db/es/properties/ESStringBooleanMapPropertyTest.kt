02f917a888c15879a5037f82acc3703da26e5350
Mauricio Muler Bodemann <mbo@scireum.de>
2024-02-07 15:11:32 +0100

    Adapts to kotlin syntax
    Fixes: OX-10357

--- src/test/kotlin/sirius/db/es/properties/ESStringBooleanMapPropertyTest.kt
+++ src/test/kotlin/sirius/db/es/properties/ESStringBooleanMapPropertyTest.kt
@@ -8,38 +8,43 @@
 
 package sirius.db.es.properties
 
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
 import sirius.db.es.Elastic
-import sirius.kernel.BaseSpecification
+import sirius.kernel.SiriusExtension
 import sirius.kernel.di.std.Part
+import kotlin.test.assertEquals
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
 
-class ESStringBooleanMapPropertySpec extends BaseSpecification {
-
-    @Part
-    private static Elastic elastic
-
-            def "read/write a string boolean map works"() {
-        when:
-        def test = new ESStringBooleanMapEntity()
-        test.getMap().put("a", true).put("b", false).put("c", new Boolean(true)).put("d", new Boolean(false))
+@ExtendWith(SiriusExtension::class)
+class ESStringBooleanMapPropertyTest {
+    @Test
+    fun `read and write a string boolean map works`() {
+        val test = ESStringBooleanMapEntity()
+        test.map.put("a", true).put("b", false).put("c", true).put("d", false)
         elastic.update(test)
-        def resolved = elastic.refreshOrFail(test)
-        then:
-        resolved.getMap().size() == 4
-        and:
-        resolved.getMap().get("a").get()
-        !resolved.getMap().get("b").get()
-        resolved.getMap().get("c").get()
-        !resolved.getMap().get("d").get()
-        when:
-        resolved.getMap().modify().remove("a")
-        and:
+        var resolved = elastic.refreshOrFail(test)
+
+        assertEquals(4, resolved.map.size())
+
+        assertTrue { resolved.map.get("a").get() }
+        assertFalse { resolved.map.get("b").get() }
+        assertTrue { resolved.map.get("c").get() }
+        assertFalse { resolved.map.get("d").get() }
+
+        resolved.map.modify().remove("a")
         elastic.update(resolved)
-        and:
         resolved = elastic.refreshOrFail(test)
-        then:
-        resolved.getMap().size() == 3
-        and:
-        !resolved.getMap().containsKey("a")
-        resolved.getMap().get("c").get()
+
+        assertEquals(3, resolved.map.size())
+
+        assertFalse { resolved.map.containsKey("a") }
+        assertTrue { resolved.map.get("c").get() }
+    }
+
+    companion object {
+        @Part
+        private lateinit var elastic: Elastic
     }
 }
