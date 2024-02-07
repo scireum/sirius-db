d9fe26657ccb86f518b183bcfb3956f57dff3dfc
Mauricio Muler Bodemann <mbo@scireum.de>
2024-02-07 15:00:21 +0100

    Adapts to kotlin syntax
    Fixes: OX-10357

--- src/test/kotlin/sirius/db/es/properties/ESNestedListPropertyTest.kt
+++ src/test/kotlin/sirius/db/es/properties/ESNestedListPropertyTest.kt
@@ -8,51 +8,48 @@
 
 package sirius.db.es.properties
 
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
 import sirius.db.es.Elastic
-import sirius.kernel.BaseSpecification
-import sirius.kernel.commons.Wait
+import sirius.kernel.SiriusExtension
 import sirius.kernel.di.std.Part
+import kotlin.test.assertEquals
 
-class ESNestedListPropertySpec extends BaseSpecification {
+@ExtendWith(SiriusExtension::class)
+class ESNestedListPropertyTest {
+    companion object {
+        @Part
+        private lateinit var elastic: Elastic
+    }
 
-    @Part
-    private static Elastic elastic
 
-            def "reading, change tracking and writing works"() {
-        when:
-        def test = new ESNestedListEntity()
-        test.getList().add(new ESNestedListEntity.NestedEntity().withValue1("X").withValue2("Y"))
+    @Test
+    fun `reading, change tracking and writing works`() {
+        val test = ESNestedListEntity()
+        test.list.add(ESNestedListEntity.NestedEntity().withValue1("X").withValue2("Y"))
         elastic.update(test)
-        elastic.refresh(ESNestedListEntity.class)
-                def resolved = elastic.refreshOrFail(test)
-                then:
-                resolved.getList().size() == 1
-                and:
-                resolved.getList().data().get(0).getValue1() == "X"
-                resolved.getList().data().get(0).getValue2() == "Y"
+        elastic.refresh(ESNestedListEntity::class.java)
+        var resolved = elastic.refreshOrFail(test)
 
-                when:
-        resolved.getList().modify().get(0).withValue1("Z")
-        and:
-        elastic.update(resolved)
-        elastic.refresh(ESNestedListEntity.class)
-                and:
-                resolved = elastic.refreshOrFail(test)
-                then:
-                resolved.getList().size() == 1
-                and:
-                resolved.getList().data().get(0).getValue1() == "Z"
-                resolved.getList().data().get(0).getValue2() == "Y"
+        assertEquals(1, resolved.list.size())
+        assertEquals("X", resolved.list.data()[0].value1)
+        assertEquals("Y", resolved.list.data()[0].value2)
 
-                when:
-        resolved.getList().modify().remove(0)
-        and:
+        resolved.list.modify()[0].withValue1("Z")
         elastic.update(resolved)
-        elastic.refresh(ESNestedListEntity.class)
-                and:
-                resolved = elastic.refreshOrFail(test)
-                then:
-                resolved.getList().size() == 0
+        elastic.refresh(ESNestedListEntity::class.java)
+        resolved = elastic.refreshOrFail(test)
+
+        assertEquals(1, resolved.list.size())
+        assertEquals("Z", resolved.list.data()[0].value1)
+        assertEquals("Y", resolved.list.data()[0].value2)
+
+        resolved.list.modify().removeAt(0)
+        elastic.update(resolved)
+        elastic.refresh(ESNestedListEntity::class.java)
+        resolved = elastic.refreshOrFail(test)
+
+        assertEquals(0, resolved.list.size())
     }
 
     /**
@@ -61,32 +58,31 @@ class ESNestedListPropertySpec extends BaseSpecification {
      * <p>
      * Otherwise an entity would match if one property in any nested object matches.
      */
-    def "searching in nested fields works as expected"() {
-        when:
-        def test = new ESNestedListEntity()
-        test.getList().add(new ESNestedListEntity.NestedEntity().withValue1("A").withValue2("B"))
-        test.getList().add(new ESNestedListEntity.NestedEntity().withValue1("C").withValue2("D"))
+    @Test
+    fun `searching in nested fields works as expected`() {
+        var test = ESNestedListEntity()
+        test.list.add(ESNestedListEntity.NestedEntity().withValue1("A").withValue2("B"))
+        test.list.add(ESNestedListEntity.NestedEntity().withValue1("C").withValue2("D"))
         elastic.update(test)
-        test = new ESNestedListEntity()
-        test.getList().add(new ESNestedListEntity.NestedEntity().withValue1("A").withValue2("B"))
-        test.getList().add(new ESNestedListEntity.NestedEntity().withValue1("A").withValue2("D"))
+        test = ESNestedListEntity()
+        test.list.add(ESNestedListEntity.NestedEntity().withValue1("A").withValue2("B"))
+        test.list.add(ESNestedListEntity.NestedEntity().withValue1("A").withValue2("D"))
         elastic.update(test)
-        elastic.refresh(ESNestedListEntity.class)
-                def query = elastic.select(ESNestedListEntity.class).
-        where(
-                Elastic.FILTERS.nested(ESNestedListEntity.LIST).eq(
-                        ESNestedListEntity.LIST.nested(ESNestedListEntity.NestedEntity.VALUE1),
-                        "A").
-                eq(ESNestedListEntity.LIST.nested(ESNestedListEntity.NestedEntity.VALUE2), "D").build())
-                def resolved = query.queryFirst()
-                then:
-                query.limit(0).count() == 1
-                resolved.getList().size() == 2
-                and:
-                resolved.getList().data().get(0).getValue1() == "A"
-                resolved.getList().data().get(0).getValue2() == "B"
-                resolved.getList().data().get(1).getValue1() == "A"
-                resolved.getList().data().get(1).getValue2() == "D"
+        elastic.refresh(ESNestedListEntity::class.java)
+        val query = elastic.select(ESNestedListEntity::class.java).where(
+                Elastic.FILTERS.nested(ESNestedListEntity.LIST)
+                        .eq(ESNestedListEntity.LIST.nested(ESNestedListEntity.NestedEntity.VALUE1), "A")
+                        .eq(ESNestedListEntity.LIST.nested(ESNestedListEntity.NestedEntity.VALUE2), "D")
+                        .build()
+        )
+        val resolved = query.queryFirst()
+
+        assertEquals(1, query.limit(0).count())
+        assertEquals(2, resolved.list.size())
+        assertEquals("A", resolved.list.data()[0].value1)
+        assertEquals("B", resolved.list.data()[0].value2)
+        assertEquals("A", resolved.list.data()[1].value1)
+        assertEquals("D", resolved.list.data()[1].value2)
     }
 
 }
